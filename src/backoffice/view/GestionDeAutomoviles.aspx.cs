cca252c3dd5d1e699196688f417396901f7c2632
Jesus <jesustrejo10@gmail.com>
2016-11-08 02:20:15 -0430

    Estructura base del proyecto

---
+++ src/backoffice/view/GestionDeAutomoviles.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace backoffice.view
+{
+    public partial class GestionDeAutomoviles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
