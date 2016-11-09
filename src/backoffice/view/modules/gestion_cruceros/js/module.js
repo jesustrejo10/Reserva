4c3b2ee68ce4ea532cfad53357ee2c645b405980
juan <juan@socialgest.net>
2016-11-09 19:05:23 -0400

    editar cabina

--- src/backoffice/view/modules/gestion_cruceros/js/module.js
+++ src/backoffice/view/modules/gestion_cruceros/js/module.js
@@ -1,5 +1,13 @@
- 
-$( document ).ready(function() { 
-    $(".datepicker").datepicker(); 
-    
-});
+ $(document).ready(function () {
+     $(".datepicker").datepicker();
+ });
+ openEdit = function (e) {
+     e.preventDefault();
+     console.log('a');
+     $('#createCabin').modal('show');
+ }
+ $(".editCabin").click(function (event) {
+     event.stopPropagation();
+     event.preventDefault();
+     $('#createCabin').modal('show');
+ });
