01c4f96d93a83f602361b26f23a2edd7284fc156
Jesus <jesustrejo10@gmail.com>
2016-11-15 02:53:01 -0430

    Agregar Vehiculo, segun nueva estructura

---
+++ src/BOReserva/Controllers/gestion_automovilesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BOReserva.Models.gestion_automoviles;
+using System.Diagnostics;
+namespace BOReserva.Controllers
+{
+    public class gestion_automovilesController : Controller
+    {
+        //
+        // GET: /gestion_automoviles/
+
+        public ActionResult M08_AgregarAutomovil()
+        {
+            CAgregarAutomovil model = new CAgregarAutomovil();
+            return PartialView(model);
+        }
+
+        [HttpPost]
+        public JsonResult saveVehicle(CAgregarAutomovil model)
+        {
+            String prueba = model._matricula;
+            Debug.WriteLine("IMPRIMIENDO UN MENSAJE"+prueba);
+            return (Json(true, JsonRequestBehavior.AllowGet));
+        }
+
+    }
+}
--- src/BOReserva/Scripts/cargadorMenu.js
+++ src/BOReserva/Scripts/cargadorMenu.js
@@ -21,4 +21,30 @@
                 }
             });
     });
+
+    $("#m08AgregarAutomovil").click(function (e) {
+        alert("hola jeffrey");
+        
+        e.preventDefault();
+        var url = '/gestion_automoviles/M08_AgregarAutomovil';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    
+    });
+
 });
