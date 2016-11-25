ae44fd89ffc3dd87f9459ddc2b241a59602157c2
Andrea <avsotoa@gmail.com>
2016-11-25 16:35:57 -0400

    Se modificó en el menú principal Vuelos, por Boletos. Se cambió estructura parcial con ajax para crear un boleto.

---
+++ src/BOReserva/Controllers/gestion_boletosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BOReserva.Controllers
+{
+    public class gestion_boletosController : Controller
+    {
+
+        public ActionResult M05_CrearBoleto()
+        {
+            return PartialView();
+        }
+
+    }
+}
--- src/BOReserva/Scripts/cargadorMenu.js
+++ src/BOReserva/Scripts/cargadorMenu.js
@@ -43,4 +43,26 @@
                 }
             });
     });
+
+    $("#m05_crearboleto").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_boletos/M05_CrearBoleto';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
 });
