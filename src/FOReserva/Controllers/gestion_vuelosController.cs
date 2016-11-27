8743a378a72fc5f6fa26b7471d61147c76fe5be1
azebac <jd.cabeza@gmail.com>
2016-11-27 16:59:04 -0400

    Transformacion a MVC del FrontOffice
    Se transformo el proyecto de FrontOffice al mismo estilo del BackOffice con el proyecto FOReserva

---
+++ src/FOReserva/Controllers/gestion_vuelosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FOReserva.Models.Vuelos;
+
+namespace FOReserva.Controllers
+{
+    public class gestion_vuelosController : Controller
+    {
+        //
+        // GET: /Vuelos/
+
+
+        public ActionResult gestion_vuelos()
+        {
+            Cvista_Vuelos model = new Cvista_Vuelos();
+            return PartialView(model);
+        }
+
+        [HttpPost]
+        public JsonResult buscarVuelo(Cvista_Vuelos model)
+        {
+            String prueba = model._prueba;
+
+            return (Json(true, JsonRequestBehavior.AllowGet));
+        }
+
+    }
+}
