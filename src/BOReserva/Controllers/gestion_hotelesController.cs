9fa00bf752079ecf1359b4074c5d327c2e8bfc37
Karolyn <lamitologica@gmail.com>
2016-12-01 19:54:37 -0400

    Aplicando MVC

---
+++ src/BOReserva/Controllers/gestion_hotelesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BOReserva.Controllers
+{
+    public class gestion_hotelesController : Controller
+    {
+        // GET: gestion_hoteles
+        public ActionResult M09_GestionHoteles_Crear()
+        {
+            return PartialView();
+        }
+
+        // GET: gestion_hoteles
+        public ActionResult M09_GestionHoteles_ModificarHotel()
+        {
+            return PartialView();
+        }
+
+        // GET: gestion_hoteles
+        public ActionResult M09_GestionHoteles_Desactivar()
+        {
+            return PartialView();
+        }
+    }
+
+
+}
--- src/BOReserva/Scripts/cargadorMenu.js
+++ src/BOReserva/Scripts/cargadorMenu.js
@@ -47,4 +47,72 @@
     
     });
 
+    $("#crearhotel").click(function (e) {
+        alert("HOTELES");
+
+        e.preventDefault();
+        var url = '/gestion_hoteles/M09_GestionHoteles_Crear';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    $("#modificarhotel").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_hoteles/M09_GestionHoteles_ModificarHotel';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    $("#desactivarhotel").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_hoteles/M09_GestionHoteles_Desactivar';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
 });
