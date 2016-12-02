14c52f7c03423f3cb32258cf2a8b6d6558fdbc9c
carlosdegois <carlosdegois@gmail.com>
2016-12-02 08:14:42 -0400

    Persistencia Usuario

--- src/BOReserva/Models/gestion_usuarios/AgregarUsuario.cs
+++ src/BOReserva/Models/gestion_usuarios/AgregarUsuario.cs
@@ -9,14 +9,25 @@ namespace BOReserva.Models.gestion_usuarios
 {
     public class AgregarUsuario
     {
+        public AgregarUsuario()
+        {
+            setFecha();
+        }
+
         /// <summary>
         /// TextBox de correo
         /// </summary>
         public String _correo { get; set; }
+
         /// <summary>
         /// TextBox de contraseña
         /// </summary>
-        public String _contraseña { get; set; }
+        public String _contraseña
+        {
+            get;
+            set;
+        }
+
         /// <summary>
         /// TextBox nombre y apellido
         /// </summary>
@@ -26,15 +37,39 @@ namespace BOReserva.Models.gestion_usuarios
         /// </summary>
         public DateTime _fechaCreacion { get; set; }
         /// <summary>
-        /// TextBox activo
+        /// Combo activo
         /// </summary>
         public String _activo { get; set; }
 
+        /// <summary>
+        /// Combo rol
+        /// </summary>
+        public int _rol { get; set; }
+
         [NotMapped]
         [Compare("_contraseña")]
-        public string _confirmarContraseña { get; set; }
-        //private Rol _rol;
+        public string _confirmarContraseña
+        {
+            get;
+            set;
+
+        }
 
 
+        public CUsuario toClass(){
+            CUsuario user = new CUsuario(_nombreApellido, _nombreApellido, HashPassword(_contraseña), _correo, _activo, _fechaCreacion, _rol);
+            return user;
+
+        }
+
+        private String HashPassword(String pass)
+        {
+            return Encriptar.CrearHash(_contraseña);
+        }
+
+        private void setFecha()
+        {
+            _fechaCreacion = DateTime.Now;
+        }
     }
 }
---
+++ src/BOReserva/Models/gestion_usuarios/ConexionBD.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.Models.gestion_usuarios
+{
+    public abstract class ConexionBD
+    {
+
+        #region Atributos
+        private SqlConnection conexion;
+        private string strConexion;
+        private SqlCommand comando;
+        private string query;
+        // cargar metodos despues de creacion del ER y mdf
+        #endregion
+
+        #region Conectar con la Base de Datos
+        /// <summary>
+        /// Metodo para realizar la conexion a la base de datos
+        /// Excepciones posibles: 
+        /// SqlException: Atrapa los errores que pueden existir en el sql server internamente
+        /// </summary>
+        public SqlConnection Conectar()
+        {
+
+            try
+            {
+                strConexion = ConfigurationManager.ConnectionStrings["DB_A1380A_reserva"].ConnectionString;
+                if (conexion == null)
+                {
+                    conexion = new SqlConnection(strConexion);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception("error en la conexion", ex);
+            }
+
+            return conexion;
+
+        }
+        #endregion
+
+        #region Desconectar de la Base de Datos
+        /// <summary>
+        /// Metodo para cerrar la sesion con la base de datos
+        /// Excepciones posibles: 
+        /// SqlException: Atrapa los errores que pueden existir en el sql server internamente
+        /// </summary>
+        public void Desconectar()
+        {
+
+            try
+            {
+                this.conexion.Close();
+            }
+
+            catch (Exception ex)
+            {
+
+
+            }
+
+        }
+
+        public void Desconectar(SqlConnection conec)
+        {
+
+            try
+            {
+                conec.Close();
+            }
+
+            catch (Exception ex)
+            {
+
+
+            }
+
+        }
+        #endregion
+
+
+        public List<ResultadoBD> EjecutarStoredProcedure(string query, List<Parametro> parametros)
+        {
+            try
+            {
+                Conectar();
+                List<ResultadoBD> resultados = new List<ResultadoBD>();
+                using (conexion)
+                {
+
+                    comando = new SqlCommand(query, conexion);
+                    comando.CommandType = CommandType.StoredProcedure;
+
+
+                    AsignarParametros(parametros);
+
+
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                    if (comando.Parameters != null)
+                    {
+                        foreach (SqlParameter parameter in comando.Parameters)
+                        {
+                            if (parameter.Direction.Equals(ParameterDirection.Output))
+                            {
+                                ResultadoBD resultado = new ResultadoBD(parameter.ParameterName,
+                                    parameter.Value.ToString());
+                                resultados.Add(resultado);
+                            }
+                        }
+                        if (resultados != null)
+                        {
+                            return resultados;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    return null;
+                }
+
+
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Desconectar();
+            }
+            return null;
+        }
+
+
+        /// <summary>
+        /// Metodo para asignar los parametros a un comando
+        /// </summary>
+        /// <param name="parametros">Lista de parametros que se le va a asociar</param>
+        public void AsignarParametros(List<Parametro> parametros)
+        {
+            foreach (Parametro parametro in parametros)
+            {
+                if (parametro != null && parametro.etiqueta != null && parametro.tipoDato != null &&
+                    parametro.esOutput != null)
+                {
+                    if (parametro.esOutput)
+                    {
+                        comando.Parameters.Add(parametro.etiqueta, parametro.tipoDato, 32000);
+                        comando.Parameters[parametro.etiqueta].Direction = ParameterDirection.Output;
+                    }
+                    else
+                    {
+                        if (parametro.valor != null)
+                        {
+                            comando.Parameters.Add(new SqlParameter(parametro.etiqueta, parametro.tipoDato, 32000));
+                            comando.Parameters[parametro.etiqueta].Value = parametro.valor;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+
+            }
+        }
+
+
+        #region Ejecutar Stored Procedure Multiples Tuplas
+        public DataTable EjecutarStoredProcedureTuplas(string query)
+        {
+            try
+            {
+                Conectar();
+                DataTable dataTable = new DataTable();
+                using (conexion)
+                {
+
+                    comando = new SqlCommand(query, conexion);
+                    comando.CommandType = CommandType.StoredProcedure;
+                    conexion.Open();
+                    using (SqlDataAdapter dataAdapter = new SqlDataAdapter(comando))
+                    {
+                        dataAdapter.Fill(dataTable);
+                        System.Diagnostics.Debug.WriteLine(dataAdapter);
+                        System.Diagnostics.Debug.WriteLine(dataTable);
+                    }
+
+                    return dataTable;
+                }
+
+
+            }
+            catch (SqlException ex)
+            {
+
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                Desconectar();
+            }
+            return null;
+        }
+        #endregion
+
+    }
+}
