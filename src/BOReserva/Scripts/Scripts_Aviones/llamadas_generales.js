eba63309bbb7d6234fa304533b51145a48e51d1a
azebac <jd.cabeza@gmail.com>
2016-12-03 01:27:18 -0400

    Agregada funcionalidad de agregar avion a la bd
    Tambien se agrega la capa de "Servicio" para encapsular las consultas sql en los proyectos BOReserva y FOReserva

---
+++ src/BOReserva/Scripts/Scripts_Aviones/llamadas_generales.js
@@ -0,0 +1,21 @@
+﻿
+    //EVENTO PARA AGREGAR UN AVION
+    $("#aceptarAvion").click(function (e) {
+        console.log("hola!");
+        e.preventDefault();
+        var form = $("#formGuardarAvion");
+        $.ajax({
+            url: "gestion_aviones/guardarAvion",
+            data: form.serialize(),
+            type: 'POST',
+            success: function (data) {
+                alert("Se guardo");
+                $('#formGuardarAvion')[0].reset();
+            }
+            , error: function (xhr, textStatus, exceptionThrown) {
+                //muestro el texto del error
+                alert(xhr.responseText);
+            }
+        });
+    });
+
