2c9fd1e90ba23c26ef0776c33c76afae3034a580
juanp22 <juan12413@gmail.com>
2016-12-04 16:33:48 -0400

    Modelo/Vista/Controlador
    clases, vista parcial, controlador

---
+++ src/FOReserva/Controllers/gestion_revisionController.cs
@@ -0,0 +1,24 @@
+﻿using FOReserva.Models.Revision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FOReserva.Controllers
+{
+    public class gestion_revisionController : Controller
+    {
+        //
+        // GET: /gestion_revision/
+
+        public ActionResult gestion_revision()
+        {
+            CRevision model = new CRevision();
+            return PartialView(model);
+
+        }
+
+
+    }
+}
