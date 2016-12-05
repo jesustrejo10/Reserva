01f7e97b6054104faebfab8bdfd9dff2ff0c8e86
EntropiaFox <the.wise.mage@gmail.com>
2016-12-05 09:42:58 -0400

    Recreación del controlador con estándares del proyecto
    Se decidión recrear el controlador de la forma en que se estandarizó en el proyecto, utilizando JsonResult para la manipulación de formularios.

---
+++ src/BOReserva/Controllers/gestion_restaurantesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BOReserva.Models.gestion_restaurantes;
+
+namespace BOReserva.Controllers
+{
+    public class gestion_restaurantesController : Controller
+    {
+        // GET: gestion_restaurantes
+        public ActionResult M10_GestionRestaurantes()
+        {
+            var model = new CRestauranteModelo();
+            return PartialView(model);
+        }
+
+        public ActionResult M10_VerRestaurantes()
+        {
+            var model = new CRestauranteModelo();
+            return PartialView(model);
+        }
+
+        [HttpPost]
+        public JsonResult saveRestaurant(CRestauranteModelo model)
+        {
+            return (Json(true, JsonRequestBehavior.AllowGet));
+        }
+    }
+}
