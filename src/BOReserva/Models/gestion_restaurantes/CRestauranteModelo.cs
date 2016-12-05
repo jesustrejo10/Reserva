c1939694fe37d6f476e45ddbc2bf1e85d99c8052
EntropiaFox <the.wise.mage@gmail.com>
2016-12-05 09:22:49 -0400

    Actualización Modelo
    Se agregaron los campos necesarios para concordar con el equivalente en FrontOffice

--- src/BOReserva/Models/gestion_restaurantes/CRestauranteModelo.cs
+++ src/BOReserva/Models/gestion_restaurantes/CRestauranteModelo.cs
@@ -7,9 +7,29 @@ namespace BOReserva.Models.gestion_restaurantes
 {
     public class CRestauranteModelo
     {
+        public int _id { get; set; }
         public string _nombre { get; set; }
         public string _tipoComida { get; set; }
         public string _direccion { get; set; }
-        public List<DateTime> _horario { get; set; }
+        public string _descripcion { get; set; }
+        public string _horarioApertura { get; set; }
+        public string _horarioCierre { get; set; }
+
+        /* Constructor por defecto */
+
+        public CRestauranteModelo() { }
+
+        /* Constructor completo */
+
+        public CRestauranteModelo(int id, string nombre, string tipoComida, string direccion, string descripcion, string horarioApertura, string horarioCierre)
+        {
+            this._id = id;
+            this._nombre = nombre;
+            this._tipoComida = tipoComida;
+            this._direccion = direccion;
+            this._descripcion = descripcion;
+            this._horarioApertura = horarioApertura;
+            this._horarioCierre = _horarioCierre;
+        }
     }
 }
