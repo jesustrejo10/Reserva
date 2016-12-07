bb24d0c038614848ca637056e76c8615f65173e7
mgangulo <gaby9415@hotmail.com>
2016-12-07 18:30:13 -0400

    BD

--- src/BOReserva/Models/gestion_seguridad_ingreso/Cgestion_seguridad_ingreso.cs
+++ src/BOReserva/Models/gestion_seguridad_ingreso/Cgestion_seguridad_ingreso.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BOReserva.Servicio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,19 +12,41 @@ namespace BOReserva.Models.gestion_seguridad_ingreso
        private String _claveCampoTexto;
        private String _nombreUsuarioTexto;
 
+       public Cgestion_seguridad_ingreso()
+       {
+       }
+        
+        public Cgestion_seguridad_ingreso(String correo, String clave, String nombre)
+        {
+            this._correoCampoTexto = correo;
+            this._claveCampoTexto = clave;
+            this._nombreUsuarioTexto = nombre;
+        }
+
         /// <summary>
         /// Funcion que verifica credenciales de usuario, se conecta con el modulo de usuario 
         /// </summary>
         /// <param name="_correoCampoTexto"> Longitud de la serie</param>
         /// <param name="_claveCampoTexto"> Longitud de la serie</param>
-        public Boolean verificarUsuario(String _correoCampoTexto, String _claveCampoTexto)
+
+        //public Boolean verificarUsuario(String _correoCampoTexto, String _claveCampoTexto)
+        public Cgestion_seguridad_ingreso verificarUsuario(String _correoCampoTexto, String _claveCampoTexto)
         {
+            M01SQL bd = new M01SQL();
+            Cgestion_seguridad_ingreso verificacion = bd.UsuarioEnBD(_correoCampoTexto, _claveCampoTexto);
+            if (verificacion._correoCampoTexto.Equals(_correoCampoTexto) && verificacion._claveCampoTexto.Equals(_claveCampoTexto))
+            {
+                return verificacion;
+            }
+            else
+                throw new Cvalidar_usuario_Exception("Usuario o contraseña incorrecto");
+            /*
             if ("admin@admin.com".Equals(_correoCampoTexto) && "123".Equals(_claveCampoTexto))
             {
                 return true;
             }
             else
-                throw new Cvalidar_usuario_Exception("Usuario o contraseña incorrecto");
+                throw new Cvalidar_usuario_Exception("Usuario o contraseña incorrecto");*/
         }
 
         public String correoCampoTexto
