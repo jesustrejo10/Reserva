e4dcb401f90392a4a236bad2d6ed2ccc3e34bd93
juanp22 <juan12413@gmail.com>
2016-12-07 10:57:33 -0400

    creando otros metodos

--- src/FOReserva/Controllers/gestion_revisionController.cs
+++ src/FOReserva/Controllers/gestion_revisionController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 
 namespace FOReserva.Controllers
 {
+
+    
     public class gestion_revisionController : Controller
     {
         //
@@ -30,32 +32,82 @@ namespace FOReserva.Controllers
 
                         //probando
         }
+        
 
         public ActionResult Consultar_Revision (string usuario)
         {
            // int search_val = Int32.Parse(Request.QueryString["search_val"]);
-            string search_txt = Request.QueryString["search_text"];
+          // string Usuario = Request.QueryString["Usuario"];
+            
+            
+            List<CRevision> lista ;  
+            
+                            
+                ManejadorSQLMuestraRevision manejador = new ManejadorSQLMuestraRevision();   
+                lista = manejador.ConsultarRevision(usuario);                              
 
-
-
-            List<CRevision> lista ;                   // mostrar revision cuando estoy en el perfil del usuario
-            if (search_txt == usuario)
-            {
-                ManejadorSQLMuestraRevision manejador = new ManejadorSQLMuestraRevision();  // crear en Servicios un manejador para listar 
-                 lista = manejador.ConsultaRevision(usuario);                               //las revisiones de ese usuario
-
-                return View(lista);
+                return PartialView(lista);
             }
 
-            else
+
+        
+        public ActionResult Eliminar_Revision(string usuario, CRevision revision)
+        {
+
+            string Usuario;
+            CRevision rev;
+            List<CRevision> lista;
+
+
+            ManejadorSQLMuestraRevision manejador = new ManejadorSQLMuestraRevision();  // crear en Servicios un manejador para listar 
+            lista = manejador.ConsultarRevision2(usuario, revision);   
+
+
+            if (lista == null)
             {
 
-                Console.Write("No se tienen revisiones");       //mostrar mensaje al usuario y no mostrar nada
-                return View(lista);
+                return PartialView(lista);
+
             }
+
+                else 
+                {
+
+              ManejadorSQLMuestraRevision manejador2 = new ManejadorSQLMuestraRevision();  // crear en Servicios un manejador para listar 
+            lista = manejador2.Eliminar_Revision(usuario, revision);   
+
+                    return PartialView(lista);
+
+                
+                }
+
+               
+            }
+
+
+       // public ActionResult Crear_Revsion()
+       // {
+
+                
+
+
+
+
+
+       // }    //  hasta aca crear Rev
+
+        }
             
         }
 
+  
+        
 
-    }
-}
+
+
+
+        
+
+
+   
+
--- src/FOReserva/Servicio/ManejadorSQLMuestraRevision.cs
+++ src/FOReserva/Servicio/ManejadorSQLMuestraRevision.cs
@@ -15,11 +15,11 @@ namespace FOReserva.Servicio
 
 
          /*Buscar Revision por Usuario*/                        // tome esto de prueba de la parte de alexander
-         public List<CRevision> buscarRestName(string restName)
+         public List<CRevision> ConsultarRevision(string restName)
          {
              string query = "Select rst_id, rst_nombre, rst_direccion From Restaurante where LOWER(rst_nombre) LIKE LOWER('%" + restName + "%')";
              SqlDataReader read = Executer(query);
-             List<CRevision> lista_rest = new List<CRevision>();
+             List<CRevision> lista_rev = new List<CRevision>();
              if (read.HasRows)
              {
                  while (read.Read())                        //agregar aca los campos de la clase CRevision
@@ -31,15 +31,61 @@ namespace FOReserva.Servicio
                      rev.Id = id;
                      rev.Name = nombre;
                      rev.Tipo = tipo;
-                     lista_rest.Add(rev);
+                     lista_rev.Add(rev);
                  }
              }
              CloseConextion();
-             return lista_rest;
+             return lista_rev;
+         }
+
+         public List<CRevision> ConsultarRevision2(string restName, CRevision revision)
+         {
+             string query = "Select rst_id, rst_nombre, rst_direccion From Restaurante where LOWER(rst_nombre) LIKE LOWER('%" + restName + "%')";
+             SqlDataReader read = Executer(query);
+             List<CRevision> lista_rev = new List<CRevision>();
+             if (read.HasRows)
+             {
+                 while (read.Read())                        //agregar aca los campos de la clase CRevision
+                 {
+                     int id = read.GetInt32(0);
+                     string nombre = read.GetString(1);
+                     string tipo = read.GetString(2);
+                     CRevision rev = new CRevision();
+                     rev.Id = id;
+                     rev.Name = nombre;
+                     rev.Tipo = tipo;
+                     lista_rev.Add(rev);
+                 }
+             }
+             CloseConextion();
+             return lista_rev;
+         }
+
+         public List<CRevision> Eliminar_Revision(string usuario, CRevision revision)
+         {
+             string query = "Select rst_id, rst_nombre, rst_direccion From Restaurante where LOWER(rst_nombre) LIKE LOWER('%" + usuario + "%' '%" + revision + "%')";
+             SqlDataReader read = Executer(query);
+             List<CRevision> lista_rev = new List<CRevision>();
+             if (read.HasRows)
+             {
+                 while (read.Read())                        //agregar aca los campos de la clase CRevision
+                 {
+                     int id = read.GetInt32(0);
+                     string nombre = read.GetString(1);
+                     string tipo = read.GetString(2);
+                     CRevision rev = new CRevision();
+                     rev.Id = id;
+                     rev.Name = nombre;
+                     rev.Tipo = tipo;
+                     lista_rev.Add(rev);
+                 }
+             }
+             CloseConextion();
+             return lista_rev;
          }
 
 
 
-
+        
     }
 }
