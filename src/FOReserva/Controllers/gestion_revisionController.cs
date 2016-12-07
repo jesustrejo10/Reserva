bf4952cfb611d21421bd3d95b84b01c29916f4b9
Carlos Herrera <carlosdhm6@gmail.com>
2016-12-07 09:49:18 -0400

    missin method consultarevision comentado

--- src/FOReserva/Controllers/gestion_revisionController.cs
+++ src/FOReserva/Controllers/gestion_revisionController.cs
@@ -35,25 +35,24 @@ namespace FOReserva.Controllers
         {
            // int search_val = Int32.Parse(Request.QueryString["search_val"]);
             string search_txt = Request.QueryString["search_text"];
-
-
-
+            
             List<CRevision> lista ;                   // mostrar revision cuando estoy en el perfil del usuario
             if (search_txt == usuario)
             {
+                
                 ManejadorSQLMuestraRevision manejador = new ManejadorSQLMuestraRevision();  // crear en Servicios un manejador para listar 
-                 lista = manejador.ConsultaRevision(usuario);                               //las revisiones de ese usuario
-
-                return View(lista);
+                //lista = manejador.ConsultaRevision(usuario);                               //las revisiones de ese usuario
+                
+                //return View(lista);
             }
 
             else
             {
 
                 Console.Write("No se tienen revisiones");       //mostrar mensaje al usuario y no mostrar nada
-                return View(lista);
+                //return View(lista);
             }
-            
+            return null;
         }
 
 
