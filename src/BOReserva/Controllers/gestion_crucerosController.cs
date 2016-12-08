d310f79c7700d80178d403f6f559a497a077c5eb
Jessica Pérez <jess27p@gmail.com>
2016-12-08 11:27:33 -0400

    Css personalizado agregado

--- src/BOReserva/Controllers/gestion_crucerosController.cs
+++ src/BOReserva/Controllers/gestion_crucerosController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,5 +20,19 @@ namespace BOReserva.Controllers
         {
             return PartialView();
         }
+
+        //public DataTable fillDataTable(string table)
+        //{
+        //    string query = "SELECT * FROM dstut.dbo." + table;
+
+        //    SqlConnection sqlConn = new SqlConnection(conSTR);
+        //    sqlConn.Open();
+        //    SqlCommand cmd = new SqlCommand(query, sqlConn);
+
+        //    DataTable dt = new DataTable();
+        //    dt.Load(cmd.ExecuteReader());
+        //    sqlConn.Close();
+        //    return dt;
+        //}
     }
 }
