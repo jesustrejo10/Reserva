950b0093dac6fd0fe290f6aad17c96ca7dd48342
Maria Jose <mariperez25>
2016-12-09 16:31:42 -0400

    Modificando Agregar nuevo

--- src/BOReserva/Models/gestion_comida_vuelo/Cagregarcomida.cs
+++ src/BOReserva/Models/gestion_comida_vuelo/Cagregarcomida.cs
@@ -7,7 +7,7 @@ namespace BOReserva.Models.gestion_comida_vuelo
 {
     public class CAgregarComida
     {
-        public String _nomnbrePlato { get; set; }
+        public String _nombrePlato { get; set; }
         public String _tipoPlato { get; set; }
         public String _estatusPlato { get; set; }
         public String _descripcionPlato { get; set; }
