66f5bd60bda1d3333d83aeb1d720474e93379b6a
Andrea <avsotoa@gmail.com>
2016-12-09 11:20:47 -0400

    Se colocaron vistas parciales de check in. OJO vista boarding pass hay que arreglarla porque se rodo el contenido

--- src/BOReserva/Scripts/cargadorMenu.js
+++ src/BOReserva/Scripts/cargadorMenu.js
@@ -223,4 +223,27 @@
             });
 
     });
+
+    $("#m05CheckIn").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_check_in/M05_CheckIn';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+
+    });
 });
