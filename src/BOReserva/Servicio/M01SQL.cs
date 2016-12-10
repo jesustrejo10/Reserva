4b6593ce87fc3a75a7cea1133ec3daf5e96f0f40
mgangulo <gaby9415@hotmail.com>
2016-12-10 00:27:38 -0400

    Validacion 3 intentos

--- src/BOReserva/Servicio/M01SQL.cs
+++ src/BOReserva/Servicio/M01SQL.cs
@@ -14,14 +14,14 @@ namespace BOReserva.Servicio
 
         public Cgestion_seguridad_ingreso UsuarioEnBD(String usuario)
         {
-            String usuarioBD = "", nombreBD = "", claveBD = "";
+            String usuarioBD = "", nombreBD = "", claveBD = "", statusBD="";
             try
             {
                 //Inicializo la conexion con el string de conexion
                 conexion = new SqlConnection(stringDeConexion);
                 //INTENTO abrir la conexion
                 conexion.Open();
-                SqlCommand cmd = new SqlCommand("Select usu_correo, usu_nombre, usu_contraseña from Usuario where usu_correo like @usu_correo", conexion);
+                SqlCommand cmd = new SqlCommand("Select usu_correo, usu_nombre, usu_contraseña, usu_activo from Usuario where usu_correo like @usu_correo AND usu_fk_rol IS NOT NULL", conexion);
                 //cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@usu_correo", usuario);
                 SqlDataReader lector = cmd.ExecuteReader();
@@ -30,10 +30,11 @@ namespace BOReserva.Servicio
                     usuarioBD = lector.GetString(0);
                     nombreBD = lector.GetString(1);
                     claveBD = lector.GetString(2);
+                    statusBD = lector.GetString(3);
                 }
                 lector.Close();
                 conexion.Close();
-                return new Cgestion_seguridad_ingreso(usuarioBD, claveBD, nombreBD);
+                return new Cgestion_seguridad_ingreso(usuarioBD, claveBD, nombreBD,statusBD);
             }
             catch (SqlException e)
             {
@@ -44,7 +45,7 @@ namespace BOReserva.Servicio
                 return null;
             }
         }
-
+/*
         public String UsuarioEstatus(String usuario)
         {
             String estatus = "";
@@ -69,7 +70,9 @@ namespace BOReserva.Servicio
             {
                 return null;
             }
-        }
+        }*/
+
+        
 
         public Boolean BloquearUsuario(String usuario)
         {            
@@ -79,7 +82,7 @@ namespace BOReserva.Servicio
                 conexion = new SqlConnection(stringDeConexion);
                 //INTENTO abrir la conexion
                 conexion.Open();
-                SqlCommand cmd = new SqlCommand("Update Usuario set usu_activo='inactivo' where usu_correo like @usu_correo", conexion);
+                SqlCommand cmd = new SqlCommand("Update Usuario set usu_activo='Inactivo' where usu_correo like @usu_correo", conexion);
                 //cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@usu_correo", usuario);
                 cmd.ExecuteNonQuery();
@@ -93,6 +96,121 @@ namespace BOReserva.Servicio
             catch (Exception e)
             {
                 return false;
+            }
+        }
+
+        public Boolean InsertarLogin(String usuario) {
+            try
+            {
+                //Inicializo la conexion con el string de conexion
+                conexion = new SqlConnection(stringDeConexion);
+                //INTENTO abrir la conexion
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("Insert into Login(log_idusuario,log_sesion,log_intentos) values((Select usu_id from Usuario where usu_correo like @usu_correo),0,0);", conexion);
+                //cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@usu_correo", usuario);
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+                return true;
+            }
+            catch (SqlException e)
+            {
+                System.Diagnostics.Debug.WriteLine("El error esta en insertarlogin");
+                return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        
+        }
+
+        public Boolean IncrementarIntentos(String usuario) {
+            try
+            {
+                //Inicializo la conexion con el string de conexion
+                conexion = new SqlConnection(stringDeConexion);
+                //INTENTO abrir la conexion
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("Update Login set log_intentos=log_intentos+1 where log_idusuario=(Select usu_id from Usuario where usu_correo like @usu_correo)", conexion);
+                //cmd.CommandType = CommandType.StoredProcedure;              
+                cmd.Parameters.AddWithValue("@usu_correo", usuario);
+                if (cmd.ExecuteNonQuery() < 1)
+                {
+                    InsertarLogin(usuario);
+                    cmd.ExecuteNonQuery();
+                }                
+                conexion.Close();
+                return true;
+            }
+            catch (SqlException e)
+            {
+                System.Diagnostics.Debug.WriteLine("El error esta en incrementar intentos");
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+                return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }        
+        }
+
+        public Boolean ResetearIntentos(String usuario)
+        {
+            try
+            {
+                //Inicializo la conexion con el string de conexion
+                conexion = new SqlConnection(stringDeConexion);
+                //INTENTO abrir la conexion
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("Update Login set log_intentos=0 where log_idusuario=(Select usu_id from Usuario where usu_correo like @usu_correo)", conexion);
+                //cmd.CommandType = CommandType.StoredProcedure;              
+                cmd.Parameters.AddWithValue("@usu_correo", usuario);
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+                return true;
+            }
+            catch (SqlException e)
+            {
+                System.Diagnostics.Debug.WriteLine("El error esta en resetear intentos");
+                return false;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public int NumeroIntentos(String usuario)
+        {
+            int intentos = 0;
+            try
+            {
+                //Inicializo la conexion con el string de conexion
+                conexion = new SqlConnection(stringDeConexion);
+                //INTENTO abrir la conexion
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("Select log_intentos from Login where log_idusuario=(Select usu_id from Usuario where usu_correo like @usu_correo)", conexion);
+                cmd.Parameters.AddWithValue("@usu_correo", usuario);
+                if (cmd.ExecuteScalar()==null)
+                {
+                    InsertarLogin(usuario);
+                }
+                else
+                    intentos = Convert.ToInt32(cmd.ExecuteScalar());
+                System.Diagnostics.Debug.WriteLine(intentos);
+                conexion.Close();
+                return intentos;
+            }
+            catch (SqlException e)
+            {
+                System.Diagnostics.Debug.WriteLine("El error esta en numero intentos");
+                return -1;
+            }
+            catch (Exception e)
+            {
+                return -1;
             }
         }
 
