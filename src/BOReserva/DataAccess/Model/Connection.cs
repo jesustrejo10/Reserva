e34eab847fb906475058fe44b806ee6c563c3514
Jesus <jesustrejo10@gmail.com>
2016-12-10 01:16:17 -0900

    Pruebas patron DAO y Singleton

---
+++ src/BOReserva/DataAccess/Model/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.DataAccess.Model
+{
+    public class Connection
+    {
+        private static SqlConnection _connection;
+        private Connection(String connetionString)
+        {
+            _connection = new SqlConnection(connetionString);
+        }
+
+        public static SqlConnection getInstance(String connectionString){
+            try
+            {
+                if (_connection == null)
+                {
+                   // _connection = new Connection(connectionString)._;
+                }
+                return null;
+            }
+            catch(SqlException e)
+            {
+                return null;
+               //imprimir mensjae y manejar error de conexion
+            }
+        }
+    }
+}
