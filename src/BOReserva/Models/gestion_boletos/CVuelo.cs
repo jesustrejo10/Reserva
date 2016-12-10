7d00fe313bd273a5f94d05da46e566d04f960242
Andrea <avsotoa@gmail.com>
2016-12-10 16:34:02 -0400

    Agregar vista de modificación de boleto

--- src/BOReserva/Models/gestion_boletos/CVuelo.cs
+++ src/BOReserva/Models/gestion_boletos/CVuelo.cs
@@ -9,8 +9,23 @@ namespace BOReserva.Models.gestion_boletos
     {
         public int _id { get; set; }
 
+        public String _origen { get; set; }
+
+        public String _destino { get; set; }
+
         public DateTime _fechaPartida { get; set; }
 
         public DateTime _fechaLlegada { get; set; }
+
+        public double _monto { get; set; }
+
+        public CVuelo(int id, string origen, string destino, DateTime fechasal, 
+                     DateTime fechalleg) {
+              _id = id;
+              _origen = origen;
+              _destino = destino;
+              _fechaPartida = fechasal;
+              _fechaLlegada = fechalleg;
+        }
     }
 }
