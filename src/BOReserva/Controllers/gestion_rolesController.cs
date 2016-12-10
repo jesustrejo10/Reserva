5b2cb09d7fbfc233661d0a96ac3980c60a00f17a
Manuel Irazabal <Manuel Irazabal>
2016-12-10 01:02:59 -0400

    instaciando clase roles

--- src/BOReserva/Controllers/gestion_rolesController.cs
+++ src/BOReserva/Controllers/gestion_rolesController.cs
@@ -13,8 +13,14 @@ namespace BOReserva.Controllers
         // GET: /gestion_roles/
         public ActionResult M13_AgregarRol()
         {
-            
             CRoles rol = new CRoles("Administrador");
+            CModulo_general modulo1 = new CModulo_general("Roles");
+            CModulo_general modulo2 = new CModulo_general("Aviones");
+            CModulo_general modulo3 = new CModulo_general("Automoviles");
+            rol.Menu.agregarElemento(modulo1);
+            rol.Menu.agregarElemento(modulo2);
+            rol.Menu.agregarElemento(modulo3);   
+           
             return PartialView(rol);
         }
         public ActionResult M13_VisualizarRol()
