242cea92012fe6a94f0fc359ddacd7f8595db0ec
Barbara Fdz <Barbara Fdz>
2016-12-10 18:45:15 -0300

    clase unitarias using

--- src/TestUnitReserva/BO/TestGestionAutomovil/TestGestionAutomovil.cs
+++ src/TestUnitReserva/BO/TestGestionAutomovil/TestGestionAutomovil.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using BOReserva.Models.gestion_automoviles;
 
 namespace TestUnitReserva.BO.TestGestionAutomovil
 {
     [TestFixture]
     public class  TestGestionAutomovil
-
-
+       
     {
+        [Test]
+        public void prueba { }
     }
 }
