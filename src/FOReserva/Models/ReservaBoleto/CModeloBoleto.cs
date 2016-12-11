edcc3fb5bd6ea5d1ecb314d4bd8e6ea4915c7506
gusth <mrgusth@gmail.com>
2016-12-11 11:28:18 -0400

    primer servicio consulta de reserva

---
+++ src/FOReserva/Models/ReservaBoleto/CModeloBoleto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FOReserva.Models.ReservaBoleto
+{
+    public class CModeloBoleto : BaseEntity
+    {
+        private string _codigo;
+        private DateTime _fecha;
+        private float _costo;
+        private string _tipo;
+
+        public CModeloBoleto
+            (int id, string name, string codigo, DateTime fecha, float costo, string tipo) : base(id, name)
+        {
+            this._codigo = codigo;
+            this._fecha = fecha;
+            this._costo = costo;
+            this._tipo = tipo;
+
+        }
+        /* Constructor Vacio */
+        public CModeloBoleto() : base (){ }
+
+        /* codigo de reserva XXXXYYYY*/
+        public string codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+
+        /* fecha de la reserva */
+        public DateTime fecha
+        {
+            get { return _fecha; }
+            set { _fecha = value; }
+        }
+
+        public float costo
+        {
+            get { return _costo; }
+            set { _costo = value; }
+        }
+
+        /* Metodos Get y Set 
+          tipo de reserva (turista,ejecutivo) */
+        public string tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+
+    }
+}
---
+++ src/FOReserva/Models/ReservaBoleto/CReservacion_Boleto.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FOReserva.Models.ReservaBoleto
+{
+    public class CReservacion_Boleto : ReservationModels
+    {
+        private int _count;
+        private UserProfile _user;
+        private int _idUser;
+        private string _codigo;
+        private DateTime _fecha;
+        private string _tipo;
+
+        /*Constructor Completo*/
+        public CReservacion_Boleto
+           (UserProfile user, string owner, DateTime date, string time, int count, int idUser, string codigo, DateTime fecha, string tipo)
+           : base( owner, date, time )
+        {
+            this._count = count;
+            this._user = user;
+            this._idUser = idUser;
+            this.codigo = _codigo;
+            this.fecha = _fecha;
+            this.tipo = _tipo;
+        }
+
+
+        /*Metodos Get y Set del Usuario de la reserva*/
+        public UserProfile User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+        /*Metodos Get y Set de la cantidad de personas de la reserva*/
+        public int Count
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+
+        public int IdUser
+        {
+            get { return _idUser; }
+            set { _idUser = value; }
+        }
+        public string codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+
+        public DateTime fecha
+        {
+            get { return _fecha; }
+            set { _fecha = value; }
+        }
+
+        public string tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+
+    }
+}
