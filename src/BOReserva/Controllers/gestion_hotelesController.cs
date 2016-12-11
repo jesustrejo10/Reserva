d5fd80b6cf5253e057f5f88d3d1aba92054927e8
Karolyn <lamitologica@gmail.com>
2016-12-11 13:57:10 -0400

    Agregado listar país y ciudad falla.

--- src/BOReserva/Controllers/gestion_hotelesController.cs
+++ src/BOReserva/Controllers/gestion_hotelesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using BOReserva.Models.gestion_hoteles;
 using BOReserva.Servicio.Servicio_Hoteles;
+using System.Diagnostics;
 
 namespace BOReserva.Controllers
 {
@@ -14,7 +15,9 @@ namespace BOReserva.Controllers
         public ActionResult M09_GestionHoteles_Crear()
         {
             CGestionHoteles_CrearHotel crear = new CGestionHoteles_CrearHotel();
-            
+
+            ViewData["pais"] = pais();
+
             return PartialView(crear);
         }
 
@@ -47,6 +50,13 @@ namespace BOReserva.Controllers
             CManejadorSQL_Hoteles pais = new CManejadorSQL_Hoteles();
             List<SelectListItem> _pais = new List<SelectListItem>();
             String[] paises = pais.MListarpaisesBD();
+
+            _pais.Add(new SelectListItem
+            {
+                Text = "Seleccione País",
+                Value = "0"
+            });
+
             int i = 0;
             bool verdad = true;
             while (verdad == true)
@@ -73,6 +83,41 @@ namespace BOReserva.Controllers
             CManejadorSQL_Hoteles buscarhoteles = new CManejadorSQL_Hoteles();
             List<CHotel> listahoteles = buscarhoteles.MListarHotelesBD();  //AQUI SE BUSCAN TODO LOS HOTELES QUE ESTAN EN LA BASE DE DATOS PARA MOSTRARLOS EN LA VISTA       
             return PartialView(listahoteles);
+        }
+
+        public List<SelectListItem> ciudad(string pais)
+        {
+            Debug.WriteLine("CIUDAD FILTRADA");
+            List<SelectListItem> _ciudades = new List<SelectListItem>();
+            CManejadorSQL_Hoteles ciudad = new CManejadorSQL_Hoteles();
+            String[] ciudadesBD = ciudad.MListarciudadesBD(pais);
+
+            _ciudades.Add(new SelectListItem
+            {
+                Text = "Seleccione Ciudad",
+                Value = "0"
+            });
+
+            int i = 0;
+            bool verdad = true;
+            while (verdad == true)
+            {
+                try
+                {
+                    _ciudades.Add(new SelectListItem
+                    {
+                        Text = _ciudades[i].ToString(),
+                        Value = i.ToString()
+                    });
+                    i++;
+                }
+                catch (Exception e)
+                {
+                    verdad = false;
+                }
+            }
+
+            return _ciudades;
         }
 
     }
