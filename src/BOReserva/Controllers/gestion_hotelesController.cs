dfd7e6b6c889b405b2b18ffc21fec7a57e6e68c1
Karolyn <lamitologica@gmail.com>
2016-12-11 19:28:32 -0400

    Commit Controller

--- src/BOReserva/Controllers/gestion_hotelesController.cs
+++ src/BOReserva/Controllers/gestion_hotelesController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using BOReserva.Models.gestion_hoteles;
 using BOReserva.Servicio.Servicio_Hoteles;
 using System.Diagnostics;
+using System.Net;
 
 namespace BOReserva.Controllers
 {
@@ -14,15 +15,17 @@ namespace BOReserva.Controllers
         // GET: gestion_hoteles
         public ActionResult M09_GestionHoteles_Crear()
         {
-            CGestionHoteles_CrearHotel crear = new CGestionHoteles_CrearHotel();
+            CHotel crear = new CHotel() {
+                _listapaises = new List<SelectListItem>(pais())
+             };
 
-            ViewData["pais"] = pais();
+            //ViewData["pais"] = pais();
 
             return PartialView(crear);
         }
 
         [HttpPost]
-        public JsonResult crearhotel(CGestionHoteles_CrearHotel crear) {
+        public JsonResult crearhotel(CHotel crear) {
             return Json(true, JsonRequestBehavior.AllowGet);
         }
 
@@ -54,7 +57,7 @@ namespace BOReserva.Controllers
             _pais.Add(new SelectListItem
             {
                 Text = "Seleccione País",
-                Value = "0"
+                Value = ""
             });
 
             int i = 0;
@@ -66,7 +69,7 @@ namespace BOReserva.Controllers
                     _pais.Add(new SelectListItem
                     {
                         Text = paises[i].ToString(),
-                        Value = i.ToString()
+                        Value =paises[i].ToString()
                     });
                     i++;
                 }
@@ -120,7 +123,45 @@ namespace BOReserva.Controllers
             return _ciudades;
         }
 
+        [HttpPost]
+        public JsonResult guardarHotel(CHotel model)
+        {
+            Debug.WriteLine(model._direccion);
+            Debug.WriteLine(model._email);
+            Debug.WriteLine(model._estrellas.ToString());
+            Debug.WriteLine(model._canthabitaciones.ToString());
+            Debug.WriteLine(model._paginaweb);
+            Debug.WriteLine("Pais "+model._pais);
+            Debug.WriteLine(model._nombre);
+
+            //Chequeo si los campos obligatorios estan vacios como medida de seguridad
+            if ((model._canthabitaciones == 0) || (model._direccion == null) || (model._nombre == null)
+                || (model._email == null) || (model._estrellas == 0)  || (model._paginaweb == null) || (model._pais == null)) 
+            {
+                //Creo el codigo de error de respuesta (OJO: AGREGAR EL USING DE SYSTEM.NET)
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                //Agrego mi error
+                String error = "Error, campo obligatorio vacio";
+                //Retorno el error
+                return Json(error);
+            }
+            //AGREGAR EL USING DEL MANEJADOR SQL ANTES (using BOReserva.Servicio; o using FOReserva.Servicio;)
+            //instancio el manejador de sql
+            CManejadorSQL_Hoteles sql = new CManejadorSQL_Hoteles();
+            //realizo el insert
+            bool resultado = sql.insertarHotel(model);
+            //envio una respuesta dependiendo del resultado del insert
+            if (resultado)
+            {
+                return (Json(true, JsonRequestBehavior.AllowGet));
+            }
+            else
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                String error = "Error insertando en la BD";
+                return Json(error);
+            }
+        }
+
     }
-
-
 }
