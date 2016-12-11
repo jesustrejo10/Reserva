1c9aa103cc1cca9f3694c14e16a728206f7cf120
DESKTOP-7EEOPD7\benjamin <bvpenaloza.11@gmail.com>
2016-12-11 14:30:35 -0400

    commit avance 1er servicio

--- src/FOReserva/Controllers/gestion_reserva_boletoController.cs
+++ src/FOReserva/Controllers/gestion_reserva_boletoController.cs
@@ -1,18 +1,27 @@
-﻿using System;
+﻿using FOReserva.Servicio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FOReserva.Models.ReservaBoleto;
 
 namespace FOReserva.Controllers
 {
     public class gestion_reserva_boletoController : Controller
     {
         //
+        private ManejadorSQLReservaBoleto _manejador = new ManejadorSQLReservaBoleto();
         // GET: /gestion_reserva_boleto/
+
         public ActionResult busqueda_parametros()
         {
-            //System.Diagnostics.Debug.WriteLine("mamalo");
+
+            IList<CModeloBoleto> boletos = _manejador.buscarReserva("Caracas", "Valencia", "2016-10-18");
+            foreach (CModeloBoleto boleto in boletos)
+                System.Diagnostics.Debug.WriteLine(boleto.codigo);
+
+
             return PartialView();
         }
         public ActionResult busqueda_resultados()
@@ -27,7 +36,5 @@ namespace FOReserva.Controllers
         {
             return PartialView();
         }
-
-
     }
 }
--- src/FOReserva/Servicio/ManejadorSQLReservaBoleto.cs
+++ src/FOReserva/Servicio/ManejadorSQLReservaBoleto.cs
@@ -16,7 +16,7 @@ namespace FOReserva.Servicio
         {
             
             //string query = "Select * from Boleto where fk_pas_if=" + pasaporte + "')";
-            string query = "Select b.bol_id, b.bol_costo from Boleto b, Lugar l1, Lugar l2 where b.bol_fk_lugar_origen = l1.lug_id and b.bol_fk_lugar_destino = l2.lug_id and l1.lug_nombre = " + origen+" and l2.lug_nombre = "+destino+" and b.bol_fecha = "+ida+"')";
+            string query = "Select b.bol_id, b.bol_costo from Boleto b, Lugar l1, Lugar l2 where b.bol_fk_lugar_origen = l1.lug_id and b.bol_fk_lugar_destino = l2.lug_id and l1.lug_nombre = '"+origen+"' and l2.lug_nombre = '"+destino+"' and b.bol_fecha = '"+ida+"'";
 
             SqlDataReader read = Executer(query);
             List<CModeloBoleto> lista_res = new List<CModeloBoleto>();
@@ -25,10 +25,12 @@ namespace FOReserva.Servicio
                 while (read.Read())
                 {
                     int id = read.GetInt32(0);
-                    float costo = read.GetFloat(1);
+                    float costo = float.Parse(read["bol_costo"].ToString()) ;
                     CModeloBoleto resv = new CModeloBoleto();
                     resv.Id = id;
                     resv.costo = costo;
+                    System.Diagnostics.Debug.WriteLine(resv.Id);
+                    System.Diagnostics.Debug.WriteLine(resv.costo);
                     lista_res.Add(resv);
                 }
             }
