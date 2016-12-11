734fc3890f8f82983b326f1b0d0ff9fdd8316a4e
Barb2908 <barbara29rp@gmail.com>
2016-12-11 16:33:46 -0400

    UsuarioSQL

--- src/FOReserva/Servicio/ManejadorSQLMuestraRevision.cs
+++ src/FOReserva/Servicio/ManejadorSQLMuestraRevision.cs
@@ -11,42 +11,37 @@ namespace FOReserva.Servicio
     public class ManejadorSQLMuestraRevision : manejadorSQL
     {
 
-
         public ManejadorSQLMuestraRevision() : base() { }
 
 
         public List<CRevision> ConsultarRevision(string nombre, string apellido)  
         {
-            string query = "Select rst_id, rst_nombre, rst_direccion From Restaurante where LOWER(nom colum a bscar) LIKE LOWER('%" + nombre + "%' '%" + apellido + "%')";
-            SqlDataReader read = Executer(query);
-            List<CRevision> lista_rev = new List<CRevision>();
-            if (read.HasRows)
-            {
-                while (read.Read())                        
-                {
-                   DateTime _fecha = read.GetDateTime(0);
-                    string _mensaje = read.GetString(1);
-                    int _tipo= read.GetInt32(2);
-                    int _puntuacion = read.GetInt32(3);
-                   // int _valoracion = read.GetInt32(4);
-                    CRevision rev = new CRevision();
-                   // CRevisionValoracion reva = new CRevisionValoracion();
-                    rev.Fecha = _fecha;
-                    rev.Mensaje = _mensaje;
-                    rev.Tipo = _tipo;
-                    rev.Puntuacion = _puntuacion;
-                  //  reva. = _valoracion;
-                    lista_rev.Add(rev);
+           
+                    string query = "SELECT us.usu_id, us.usu_activo, rev.rev_fecha, rev.rev_mensaje, rev.rev_tipo, rev.rev_puntuacion, val.rv_val_pos, val.rv_val_neg FROM Revision as rev, Revision_Valoracion as val, Usuario as us where val.rv_id=rev.rev_FK_rev_val_id and LOWER(us.usu_nombre) LIKE LOWER('%" + nombre + "%') and LOWER(us.usu_apellido) LIKE LOWER('%" + apellido + "%') and rev.rev_FK_usu_id=us.usu_id";
+                    SqlDataReader read = Executer(query);
+                    List<CRevision> lista_rev = new List<CRevision>();
+                    if (read.HasRows)
+                    {
+                        while (read.Read())                        
+                        {
+                           DateTime _fecha = read.GetDateTime(0);
+                            string _mensaje = read.GetString(1);
+                            int _tipo= read.GetInt32(2);
+                            int _puntuacion = read.GetInt32(3);
+                           // int _valoracion = read.GetInt32(4);
+                            CRevision rev = new CRevision();
+                           // CRevisionValoracion reva = new CRevisionValoracion();
+                            rev.Fecha = _fecha;
+                            rev.Mensaje = _mensaje;
+                            rev.Tipo = _tipo;
+                            rev.Puntuacion = _puntuacion;
+                          //  reva. = _valoracion;
+                            lista_rev.Add(rev);
+                       
+                  
+                         }
+                    }
 
-                     
-               
-       
-            
-        
-
-
-                }
-            }
             CloseConnection();
             return lista_rev;
         }
