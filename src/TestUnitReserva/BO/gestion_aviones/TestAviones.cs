b9a6fc7c29835e61a1a69c878d7a1a499bc6a6ea
Valeria Vargas <vavargas.12@gmail.com>
2016-12-11 21:04:06 -0400

    Migracion de pruebas unitaria a NUnit

--- src/TestUnitReserva/BO/gestion_aviones/TestAviones.cs
+++ src/TestUnitReserva/BO/gestion_aviones/TestAviones.cs
@@ -7,17 +7,20 @@ using NUnit.Framework;
 using BOReserva.Models.gestion_aviones;
 using BOReserva.Servicio;
 using System.Diagnostics;
+using System.Data.SqlClient;
 
 namespace TestUnitReserva.BO.gestion_aviones
 {
     [TestFixture]
     class TestAviones
     {
+        manejadorSQL prueba = new manejadorSQL();
+
         [Test]
         public void M02_InsertarAvion()
         {
             CAgregarAvion avion = new CAgregarAvion();
-            avion._matriculaAvion="Hk900";
+            avion._matriculaAvion = BOReserva.Models.Util.RandomString(7);
             avion._modeloAvion = "Boeing700";
             avion._capacidadPasajerosTurista = 100;
             avion._capacidadPasajerosEjecutiva = 100;
@@ -26,7 +29,6 @@ namespace TestUnitReserva.BO.gestion_aviones
             avion._distanciaMaximaVuelo = 200;
             avion._velocidadMaximaDeVuelo = 1000;
             avion._capacidadMaximaCombustible = 100;
-            manejadorSQL prueba = new manejadorSQL();
             //Aquí pruebo que al insertar un avión de manera correcta la función devuelva true
             Boolean resultadoconavion = prueba.insertarAvion(avion);
             Assert.AreEqual(resultadoconavion, true);
@@ -41,6 +43,70 @@ namespace TestUnitReserva.BO.gestion_aviones
             avion._matriculaAvion = "Hk900";
             Boolean resultadomatricularepetida = prueba.insertarAvion(avion);
             Assert.AreEqual(resultadomatricularepetida, false);    
+          
         }
+
+        [Test]
+        public void M02_ListarAvionesEnBd()
+        {
+            //pruebo que la lista no retorne vacia
+            List<CAvion> respuesta = prueba.listarAvionesEnBD();
+            Assert.IsNotNull(respuesta);
+        }
+
+        [Test]
+        public void consultarAvionIdInvalida()
+        {
+            int numeroNulo = 31211;
+            Assert.IsNotInstanceOf(typeof(CAvion) ,prueba.consultarAvion(numeroNulo));
+        }
+
+        [Test]
+        public void consultarAvionIdValida()
+        {
+            int numeroValido = 1;
+            Assert.IsInstanceOf( typeof(CAvion) , prueba.consultarAvion(numeroValido));
+        }
+
+   
+        [Test]
+        public void modificarAvionInexistente()
+        {
+            CModificarAvion avion = new CModificarAvion();
+            avion._matriculaAvion = "HK-45";
+            avion._modeloAvion = "Boeing700";
+            avion._capacidadPasajerosTurista = 100;
+            avion._capacidadPasajerosEjecutiva = 100;
+            avion._capacidadPasajerosVIP = 100;
+            avion._capacidadEquipaje = 100;
+            avion._distanciaMaximaVuelo = 200;
+            avion._velocidadMaximaDeVuelo = 1000;
+            avion._capacidadMaximaCombustible = 100;
+            Boolean resultadoModificar = prueba.modificarAvion(avion);
+            Assert.AreEqual(resultadoModificar, true);
+        }
+
+        [Test]
+        public void verificarDeshabilitarAvion()
+        {
+            Boolean resultado = prueba.deshabilitarAvion(1);
+            Assert.AreEqual(resultado, true);
+        }
+
+        [Test]
+        public void verificarHabilitarAvion()
+        {
+            Boolean resultado = prueba.habilitarAvion(1);
+            Assert.AreEqual(resultado, true);
+       }
+
+        [Test]        
+        public void modificarAvionNulo()
+        {
+            Assert.That(() => prueba.modificarAvion(null),
+            Throws.TypeOf<SqlException>());
+
+        } 
+        
     }
 }
