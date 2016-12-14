e030ae04262a55f5c6967d2b50e7586f03198d34
Jessica Pérez <jess27p@gmail.com>
2016-12-14 01:39:21 -0400

    archivos nuevos

---
+++ src/BOReserva/Models/gestion_check_in/CRuta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.Models.gestion_check_in
+{
+    public class CRuta
+    {
+        public int _id { get; set; }
+
+        public int _origen { get; set; }
+
+        public int _destino { get; set; }
+
+        public String _nomOrigen { get; set; }
+
+        public String _nomDestino { get; set; }
+
+        public CRuta(int id, int ori, int dest, String nombre1, String nombre2)
+        {
+            _id = id;
+            _origen = ori;
+            _destino = dest;
+            _nomOrigen = nombre1;
+            _nomDestino = nombre2;
+        }
+    }
+}
---
+++ src/BOReserva/Models/gestion_check_in/CVuelo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.Models.gestion_check_in
+{
+    public class CVuelo
+    {
+        public int _id { get; set; }
+
+        public CRuta _ruta { get; set; }
+
+        public DateTime _fechaPartida { get; set; }
+
+        public DateTime _fechaLlegada { get; set; }
+
+        public double _monto { get; set; }
+
+        public CVuelo(int num_id, DateTime fechasal, DateTime fechalleg, int ruta_id, int ori, int dest, String nombre1, String nombre2)
+        {
+            _id = num_id;
+            _fechaPartida = fechasal;
+            _fechaLlegada = fechalleg;
+            _ruta = new CRuta(ruta_id, ori, dest, nombre1, nombre2);
+        }
+
+        public CVuelo()
+        {
+        }
+    }
+}
