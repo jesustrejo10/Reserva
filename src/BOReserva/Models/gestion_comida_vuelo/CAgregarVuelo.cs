5758bf78c9cbd7409d30f8e9bca9a13fc217187a
mariperez25 <mariperez259@gmail.com>
2016-12-14 19:15:36 -0400

    visualizar vuelos

---
+++ src/BOReserva/Models/gestion_comida_vuelo/CAgregarVuelo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.Models.gestion_comida_vuelo
+{
+    public class CAgregarVuelo
+    {
+        public string _codigoVuelo { get; set; }
+        public string _ciudadOrigen { get; set; }
+        public string _ciudadDestino { get; set; }
+        public int _capacidadTurista { get; set; }
+        public int _capacidadEjecutiva { get; set; }
+        public int _capacidadVip { get; set; }
+    }
+}
