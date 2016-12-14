62472a9a0e91a963c082ad6a79df8f73cf8bb1b2
Luisma93 <luismared93@gmail.com>
2016-12-14 11:20:36 -0400

    Agregando pruebas unitarias (faltan)

---
+++ src/TestUnitReserva/BO/gestion_vuelos/TestVuelo.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BOReserva.Models.gestion_vuelo;
+using BOReserva.Servicio;
+using BOReserva.Servicio.Servicio_Vuelos;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace TestUnitReserva.BO.gestion_vuelos
+{
+    [TestFixture]
+    class TestVuelo
+    {
+        manejadorSQL_Vuelos sql = new manejadorSQL_Vuelos();
+        String Codigo = "AA123";
+        String Origen = "Caracas";
+        String Destino = "Maracaibo";
+        String Despegue = "2016-12-15 09:00:00";
+        String Status = "activo";
+        String Aterrizaje = "2016-12-15 10:10:00";
+        String Matricula = "1";
+
+
+        [Test]
+        public void M04_CrearVuelo()
+        {
+            Boolean resultado = sql.insertarVuelo(Codigo, Origen, Destino, Despegue, Status, Aterrizaje, Matricula);
+
+            //Aquí pruebo que al insertar un vuelo de manera correcta la función devuelva true
+            Assert.Equals(resultado, true);
+            //Aquí pruebo que el insertar vuelo no me deje agregar atributos vacíos
+            Boolean resultadoconnull = sql.insertarVuelo(null, null, null, null, null, null, null);
+            Assert.AreEqual(resultadoconnull, false);
+            //Aquí pruebo que no me deje insertar una codigo de vuelo repetido
+            String Codigo2 = "V507";
+            Boolean resultadocodigorepetido = sql.insertarVuelo(Codigo2, Origen, Destino, Despegue, Status, Aterrizaje, Matricula);
+            Assert.AreEqual(resultadocodigorepetido, false);
+        }
+
+        [Test]
+        public void M04_BuscarIdRutaVuelo()
+        {
+
+            int busqueda = sql.idRutaVuelo(Origen, Destino);
+            //Pruebo que al buscar una ruta con una ciudad de origen y una ciudad destino dada me devuelva un id existente.
+            Assert.NotNull(busqueda);
+        }
+
+        [Test]
+        public void M04_BuscarCiudadesDestino()
+        {
+
+            List<CCrear_Vuelo> consulta = sql.consultarDestinos(Origen);
+            //Pruebo que al buscar una lista de ciudades destino de una ciudad origen dada me devuelva una lista existente.
+            Assert.IsNotNull(consulta);
+            // Ya se que una de las ciudades destino de Caracas es Maracaibo
+            //Assert.AreEqual(consulta, "Maracaibo");
+
+        }
+        [Test]
+        public void M04_BuscarAviones()
+        {
+            List<CCrear_Vuelo> buscar = sql.buscarAviones(Origen, Destino);
+            //Pruebo que al ingresar una cuidad origen y una ciudad destino me devuelva una lista existente de aviones que cubran la distancia de dicha ruta.
+            Assert.IsNotNull(buscar);
+            //Ya se que la matricula de uno de los aviones que cubre la ruta de Caracas a Maracaibo es IAM-123
+            //Assert.AreEqual(buscar, "IAM-123");
+        }
+
+        [Test]
+        public void M04_CargarOrigenes()
+        {
+            List<CCrear_Vuelo> cargar = sql.cargarOrigenes();
+            // Pruebo que al cargar un Origen no me retorne una lista nula.
+            Assert.IsNotNull(cargar);
+        }
+
+        [Test]
+        public void M04_Detalle_Avion_Modelo()
+        {
+            string matriculaAvion = "IAM-123";
+            string modelo = sql.modeloAvion(matriculaAvion);
+            //verifico que modelo de avion le corresponde a la martricula IAM-123 el cual es Boeing 777.
+            Assert.AreEqual(modelo, "Boeing 777");
+            //Verifico que no me traiga un modelo nulo.
+            Assert.IsNotNull(modelo);
+        }
+
+        [Test]
+        public void M04_DetalleAvion_Pasajeros()
+        {
+            string matriculaAvion = "IAM-123";
+            string pasajeros = sql.pasajerosAvion(matriculaAvion);
+            //verifico la capacidad maxima del avion con matricula IAM-123 la cual es 40
+            Assert.AreEqual(pasajeros, 40);
+            //Verifico que no me traiga una capacidad nula.
+            Assert.IsNotNull(pasajeros);
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
