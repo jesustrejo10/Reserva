5dc115e0faad280001af1c546806a49481f7dd02
Roysbert Salinas <rasc.19@gmail.com>
2016-12-14 18:48:17 -0400

    Integracion con perfil

--- src/FOReserva/Scripts/cargadorPestanas.js
+++ src/FOReserva/Scripts/cargadorPestanas.js
@@ -18,9 +18,9 @@ function cargarContenido(seccion, tipo, url, data, boton) {
 
     if (seccion == '#MD' || seccion == '#MI') {
         mostrarContenedor(1)
-    } else if (seccion == '#CI') {
+    }/* else if (seccion == '#CI') {
         mostrarContenedor(3)
-    } else if (seccion == '#CP') {
+    }*/ else if (seccion == '#CP') {
         mostrarContenedor(2)
     }
 
@@ -28,6 +28,8 @@ function cargarContenido(seccion, tipo, url, data, boton) {
         seccion = "#contenedor"
     else if (seccion == "#MI")
         seccion = "#contenedorImagenes"
+    else if (seccion == "#CI")
+        seccion = "#contenedorPerfil"
 
     $(seccion)
         .empty()
