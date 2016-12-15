4fa9e5316ee5867daabe4ecc0de63294a2cc55ec
Mario Salazar <mario.rsv2@gmail.com>
2016-12-14 20:49:25 -0400

    Subiendo de nuevo

--- src/FOReserva/Controllers/gestion_reserva_cruceroController.cs
+++ src/FOReserva/Controllers/gestion_reserva_cruceroController.cs
@@ -8,59 +8,84 @@ using FOReserva.Servicio;
 
 
 namespace FOReserva.Controllers
-{
+{/**
+  * Clase Controlador de Gestion para la Reserva de Crucero
+  * 
+  * 
+  * 
+  * **/
     public class gestion_reserva_cruceroController : Controller
     {
-        //
-        // GET: /gestion_reserva_crucero/
-
+        /**
+  * Metodo Gestion Reserva Crucero
+  * Este devuelve los cruceros en el ComboBox de la vista parcial en el index de la pestaña cruceros
+  * 
+  * **/
         public ActionResult gestion_reserva_crucero()
         {
             CVista_Cruceros model = null;
+            ///Se instancia un try para la consulta a la base de datos
             try { 
             model = new CVista_Cruceros();
             return PartialView(model);
             }
+                ///Se atrapa las Exception de Tipo NullReference
             catch (NullReferenceException e)
             {
 
                 return PartialView(model);
             }
+            ///Se atrapa las Exception de Tipo ManejadorSQL Exception
             catch (ManejadorSQLException f)
             {
 
                 return View("gestion_reserva_crucero_error_conexion");
             }
+            ///Se atrapa las Exception de Tipo Exception
             catch (Exception g)
             {
               
                 return PartialView(model);
             }
            }
-
+        /**
+  * Metodo Gestion Reserva Crucero Imagenes
+  * Este devuelve las imagenes  vista parcial en el index de la pestaña cruceros
+  * 
+  * **/
         public ActionResult gestion_reserva_cruceroImagenes()
         {
             return PartialView();
         }
+
+        /**
+ * Metodo Gestion Reserva Crucero Confirmar
+ * Este devuelve si la reserva fue crea con exito
+ * 
+ * **/
         public ActionResult gestion_reserva_crucero_confirmar(string id_crucero, string id_origen, string id_destino, string id_inicio, string id_fin, string pasajeros_num, string fk_ruta, string fk_crucero)
         {
             CReserva_Cruceros reserva = new CReserva_Cruceros(id_crucero, id_origen, id_destino, fk_crucero, id_inicio, id_fin, pasajeros_num, fk_ruta);
             manejadorSQLCrucero manejador = new manejadorSQLCrucero();
+            ///Se instancia un try para la consulta a la base de datos
             try
             {
 
                 manejador.CrearReserva(reserva);
                 return View();
             }
+            ///Se atrapa las Exception de Tipo NullReference
             catch (NullReferenceException e)
             {
 
                 return View();
             }
+            ///Se atrapa las Exception de Tipo ManejadorSQL Exception
             catch (ManejadorSQLException f)
             {
                 return View("gestion_reserva_crucero_error_conexion");
             }
+            ///Se atrapa las Exception de Tipo Exception
             catch (Exception g)
             {
 
@@ -68,6 +93,11 @@ namespace FOReserva.Controllers
             return View();
 
         }
+        /**
+* Metodo Gestion Reserva Crucero Perfil
+* Este metodo llena la datatable que esta contenido en la seccion Mis Cruceros en la pagina de perfil
+* 
+* **/
         public ActionResult gestion_reserva_crucero_perfil()
         {
             List<CReserva_Cruceros> lista = null;
@@ -77,6 +107,11 @@ namespace FOReserva.Controllers
             return PartialView(lista);
            
         }
+        /**
+* Metodo Gestion Reserva Crucero Buscar Crucero
+* Este metodo realiza la consulta en la base de datos de los cruceros que son solicitados por el cliente
+* 
+* **/
         public ActionResult gestion_reserva_crucero_buscar_crucero()
         {
 
@@ -84,19 +119,23 @@ namespace FOReserva.Controllers
             string crucero = Request.Form["SelectedCrucero"].ToString();
             string fechaida = Request.Form["_ida"].ToString();
             string fechavuelta = Request.Form["_vuelta"].ToString();
+            ///Se instancia un try para la consulta a la base de datos
             try {
                 List<CReserva_Cruceros> Model = busqueda(crucero, fechaida, fechavuelta);
                  return View(Model);
             }
+            ///Se atrapa las Exception de Tipo NullReference
               catch (NullReferenceException e)
             {
                 List<CReserva_Cruceros> Model = null;
                 return View(Model);
             }
+            ///Se atrapa las Exception de Tipo ManejadorSQL Exception
             catch (ManejadorSQLException f)
             {
                 return View("gestion_reserva_crucero_error_conexion");
             }
+            ///Se atrapa las Exception de Tipo Exception
             catch (Exception g)
             {
                 List<CReserva_Cruceros> Model = null;
@@ -105,9 +144,13 @@ namespace FOReserva.Controllers
         }
 
 
-        
+        /**
+* Metodo para llenar la Lista que sera retornada para la vista parcial
+* Este metodo realiza la consulta en la base de datos de los cruceros que son solicitados por el cliente
+* 
+* **/
 
-        private List<CReserva_Cruceros> busqueda(string numcrucero, string fechaIda, string fechaVuelta)
+        public List<CReserva_Cruceros> busqueda(string numcrucero, string fechaIda, string fechaVuelta)
         {
             List<CReserva_Cruceros> lista = null;
             var manejador = new manejadorSQLCrucero();
@@ -118,25 +161,37 @@ namespace FOReserva.Controllers
           
             
         }
-        
+        /**
+* Metodo para la Gestion de  Reserva de  Crucero-Resultado
+* Este metodo pasa de la ventana  Gestion de  Reserva de  Crucero-Buscar Resultado a la ventana Gestion de  Reserva de  Crucero-Resultado
+* 
+* **/
 
         public ActionResult gestion_reserva_crucero_resultado_crucero(string id_crucero, string id_origen, string id_destino, string id_inicio, string id_fin, string fk_ruta, string fk_crucero)
         {
+            ///Se instancia un try para la consulta a la base de datos
              try{
             CReserva_Cruceros reserva = new CReserva_Cruceros(fk_crucero, id_origen, id_destino, id_crucero, id_inicio, id_fin, fk_ruta);
             return View(reserva);
              }
+             ///Se atrapa las Exception de Tipo NullReference
               catch (NullReferenceException e)
             {
                 CReserva_Cruceros reserva = null;
                 return View(reserva);
             }
+             ///Se atrapa las Exception de Tipo ManejadorSQL Exception
             catch (ManejadorSQLException f)
             {
                 return View("gestion_reserva_crucero_error_conexion");
             }
             
         }
+        /**
+* Metodo para la Busqueda de Cruceros
+* 
+* **/
+
         
         [HttpPost]
         public JsonResult buscarCruceros(CVista_Cruceros model)
@@ -148,26 +203,33 @@ namespace FOReserva.Controllers
             return (Json(true, JsonRequestBehavior.AllowGet));
 
         }
+        /**
+* Metodo para la Creacion de la reserva 
+* 
+* **/
 
         public ActionResult crearReservaCrucero( string fecha, int cantidadPasajeros, int usuario, int crucero, int ruta, string fkfecha, string estatus)
         {
             CReserva_Cruceros reserva = new CReserva_Cruceros(fecha, cantidadPasajeros, usuario, crucero, ruta, fkfecha, estatus);
-
+            ///Se instancia un try para la consulta a la base de datos
             try
             {
                 manejadorSQLCrucero manejador = new manejadorSQLCrucero();
                 manejador.CrearReserva(reserva);
                 return View(reserva);
             }
+            ///Se atrapa las Exception de Tipo NullReference
             catch (ManejadorSQLException e)
             {
                 return View("gestion_reserva_crucero_error_conexion");
             }
+            ///Se atrapa las Exception de Tipo ManejadorSQL Exception
             catch (InvalidManejadorSQLException f)
             {
                 reserva = null;
                 return View(reserva);
             }
+            ///Se atrapa las Exception de Tipo Exception
             catch (Exception e)
             {
                 reserva = null;
@@ -176,50 +238,58 @@ namespace FOReserva.Controllers
         }
 
         /*
-         * Metodo para la eliminacion de la reserva
+         * Metodo para la eliminacion de la reserva de Crucero
          */
         [System.Web.Services.WebMethod]
         public JsonResult eliminarReservaCrucero(int id)
-        {
+        {///Se instancia un try para la consulta a la base de datos
             try
             {
                 manejadorSQLCrucero manejador = new manejadorSQLCrucero();
                 manejador.eliminarReserva(id);
             }
+            ///Se atrapa las Exception de Tipo ManejadorSQL Exception
             catch (ManejadorSQLException e)
             {
 
                 return null;
             }
+            ///Se atrapa las Exception de Tipo Invalid ManejadorSQL Exception
             catch (InvalidManejadorSQLException e)
             {
                 return null;
             }
+            ///Se atrapa las Exception de Tipo Exception
             catch (Exception e)
             {
              
             }
             return Json("exito");
         }
-
+        /*
+         * Metodo para la modificacion de la reserva de Crucero
+         */
         public JsonResult modificarReservaCrucero(string id_reserva, string cant_pasajero, string estatus)
         {
-         
 
+            ///Se instancia un try para la consulta a la base de datos
             try
             {
                 manejadorSQLCrucero manejador = new manejadorSQLCrucero();
                 manejador.modificarReserva(id_reserva, cant_pasajero, estatus);
             }
+            ///Se atrapa las Exception de Tipo Invalid ManejadorSQL Exception
             catch (ManejadorSQLException e)
             {
                
                 return null;
             }
+            ///Se atrapa las Exception de Tipo Invalid ManejadorSQL Exception
             catch (InvalidManejadorSQLException e)
             {
                
             }
+            ///Se atrapa las Exception de Tipo Exception
             catch (Exception e)
             {
              
@@ -229,8 +299,6 @@ namespace FOReserva.Controllers
             return Json("exito");
 
         }
-
-        /* Metodo que devuelve todas las reservas del usuario */
 
 }
     }
