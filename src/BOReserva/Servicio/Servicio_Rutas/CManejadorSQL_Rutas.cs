23985f47dee9d1ec150f9bfd4b457b255f1606ad
Daniel Moncadaz <Danielmoncadaz93@gmail.com>
2016-12-15 20:17:24 -0400

    Agarrando String de Conexion de la clase ManejadorSQL.cs

--- src/BOReserva/Servicio/Servicio_Rutas/CManejadorSQL_Rutas.cs
+++ src/BOReserva/Servicio/Servicio_Rutas/CManejadorSQL_Rutas.cs
@@ -27,49 +27,6 @@ namespace BOReserva.Servicio.Servicio_Rutas
             con.Open();
         }
 
-        public Boolean MAgregarRuta(CAgregarRuta model)
-        {
-            try
-            {
-
-                String[] strDes = model._destinoRuta.Split(new[] { " - " }, StringSplitOptions.None);
-                String[] strOri = model._origenRuta.Split(new[] { " - " }, StringSplitOptions.None);
-
-                con = new SqlConnection(connectionString);
-                con.Open();
-
-                SqlCommand query = new SqlCommand("M03_AgregarRuta", con);
-                query.CommandType = CommandType.StoredProcedure;
-
-                query.Parameters.Add("@ciudadOrigenRuta", SqlDbType.VarChar).Value = strOri[0];
-                query.Parameters.Add("@paisOrigenRuta", SqlDbType.VarChar).Value = strOri[1];
-                query.Parameters.Add("@ciudadDestinoRuta", SqlDbType.VarChar).Value = strDes[0];
-                query.Parameters.Add("@paisDestinoRuta", SqlDbType.VarChar).Value = strDes[1];
-                query.Parameters.Add("@tipoRuta", SqlDbType.VarChar).Value = model._tipoRuta;
-                query.Parameters.Add("@estadoRuta", SqlDbType.VarChar).Value = model._estadoRuta;
-                query.Parameters.Add("@distanciaRuta", SqlDbType.Int).Value = model._distanciaRuta;
-
-                query.ExecuteNonQuery();
-
-                //creo un lector sql para la respuesta de la ejecucion del comando anterior               
-                SqlDataReader lector = query.ExecuteReader();
-
-                lector.Close();
-
-                return true;
-
-            }
-            catch (SqlException e)
-            {
-                throw e;
-            }
-            catch (Exception e)
-            {
-                return false;
-            }
-        }           
-
-
         public List<CRuta> MListarRutasBD()
         {
             List<CRuta> listarutas = new List<CRuta>();
