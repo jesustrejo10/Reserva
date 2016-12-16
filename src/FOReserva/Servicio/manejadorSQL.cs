e287b9b9916c98b2d12a2d64fb7719403d1fad94
Carlos Herrera <carlosdhm6@gmail.com>
2016-12-15 23:11:04 -0400

    add using manejadorSQL

--- src/FOReserva/Servicio/manejadorSQL.cs
+++ src/FOReserva/Servicio/manejadorSQL.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 //IMPORTANTE AGREGAR EL USING DE SUS RESPECTIVAS CLASES PARA PODER AGREGAR EL METODO DE AGREGAR/CONSULTAR
@@ -54,6 +55,39 @@ namespace FOReserva.Servicio
             SqlCommand execute = this.Conexion.CreateCommand();
             execute.CommandText = query;
             SqlDataReader tmp = null;
+            try
+            {
+                tmp = execute.ExecuteReader();
+            }
+            catch (SqlException e)
+            {
+                throw new ManejadorSQLException("Error de conexion con la DB", e);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new ManejadorSQLException("Operacion invalida en la DB", e);
+            }
+            return tmp;
+        }
+
+        /*Metodo para la accion de un query parametrizado con lista de SqlParameter*/
+        public SqlDataReader Executer(string query, List<SqlParameter> parametros)
+        {
+            conexion = new SqlConnection(stringDeConexion);
+            SqlCommand execute = new SqlCommand(query, conexion);
+            foreach(SqlParameter p in parametros){
+                execute.Parameters.Add(p);
+            }
+            SqlDataReader tmp = null;
+            conexion.Open();
+
+            //Para probar el query final
+            string aaa = execute.CommandText;
+            foreach (SqlParameter p in execute.Parameters)
+            {
+                aaa = aaa.Replace(p.ParameterName, p.Value.ToString());
+            }
+            
             try
             {
                 tmp = execute.ExecuteReader();
