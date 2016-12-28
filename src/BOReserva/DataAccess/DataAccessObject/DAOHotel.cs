9429dd34836cdd1254b835ca80e0c8c942959daa
Jesus <jesustrejo10@gmail.com>
2016-12-28 14:44:49 -0430

    DAO, IDAO, Fabrica DAO

---
+++ src/BOReserva/DataAccess/DataAccessObject/DAOHotel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.DataAccess.DAO
+{
+    public class DAOHotel: DAO {
+
+        /* Metodos del Contrato IDAOHotel*/
+        private String t;
+
+        public DAOHotel() {
+            
+        }
+
+        public int Insertar(Domain.Entidad e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Entidad Actualizar(Domain.Entidad e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Entidad Consultar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Entidad Consultar(Domain.Entidad e)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
