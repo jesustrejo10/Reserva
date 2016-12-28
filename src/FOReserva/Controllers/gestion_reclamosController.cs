e9bdf78401fd6bc16b40d64ce11b8fb238c32fe8
karlimsc <karlianamsuarez@gmail.com>
2016-12-28 14:07:57 -0400

    Ya estamos en el menú de Perfil

---
+++ src/FOReserva/Controllers/gestion_reclamosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FOReserva.Controllers
+{
+    public class gestion_reclamosController : Controller
+    {
+        //
+        // GET: /gestion_reclamos/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/FOReserva/Models/Reclamos/CAgregarReclamo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FOReserva.Models.Reclamos
+{
+    public class CAgregarReclamo
+    {
+        public String _tituloReclamo { get; set; }
+        public String _detalleReclamo { get; set; }
+    }
+}
