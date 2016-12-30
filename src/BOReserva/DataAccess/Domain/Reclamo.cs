5bc354426b377f13284ff45cb7aec8089a482740
karlimsc <karlianamsuarez@gmail.com>
2016-12-29 22:00:14 -0400

    Solo agregué la clase Reclamo al Dominio

---
+++ src/BOReserva/DataAccess/Domain/Reclamo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.DataAccess.Domain
+{
+    public class Reclamo : Entidad
+    {
+        public String _tituloReclamo { get; set; }
+        public String _detalleReclamo { get; set; }
+        public String _fechaReclamo { get; set; }
+        public String _estadoReclamo { get; set; }
+    }
+
+}
