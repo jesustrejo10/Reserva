2af4f6d78d056c56bcd3ced6437462bdd1253554
drbrstudent <drbr20@hotmail.com>
2017-01-13 18:55:32 -0400

    Cambios Vista Agregar

--- src/BOReserva/Controllers/gestion_restaurantesController.cs
+++ src/BOReserva/Controllers/gestion_restaurantesController.cs
@@ -18,21 +18,9 @@ namespace BOReserva.Controllers
     {
 
 
-        private CRestauranteModelo restaurant;
+      
 
-        public CRestauranteModelo Restaurant
-        {
-            get
-            {
-                return restaurant;
-            }
-
-            set
-            {
-                restaurant = value;
-            }
-        }
-
+        #region Consultar Restaurantes por ciudad
         // GET: gestion_restaurantes
 
         /// <summary>
@@ -42,11 +30,10 @@ namespace BOReserva.Controllers
         public ActionResult M10_GestionRestaurantes_Ver(int id = 0)
         {
             ViewBag.Ciudad = FabricaVista.asignarItemsComboBox(cargarComboBoxLugar(), "Id", "Nombre");
-              
-
+        
             if (id != 0)
             {
-                System.Diagnostics.Debug.WriteLine("el id del restaurant "+id);
+               
                 Entidad _lugar = FabricaEntidad.inicializarLugar(id, "");//Aqui se envia la clave foranea de lugar para realizar la busqueda
                 Comando<List<Entidad>> comando = (Comando<List<Entidad>>)FabricaComando.comandosRestaurant(FabricaComando.comandosGlobales.CONSULTAR, _lugar);
                 List<Entidad> restaurantes = comando.Ejecutar();
@@ -56,18 +43,17 @@ namespace BOReserva.Controllers
                 foreach (Entidad item in restaurantes)
                 {
                     lista.Add((CRestauranteModelo)item);
-                    System.Diagnostics.Debug.WriteLine("el Nombre del restaurant " + ((CRestauranteModelo)item).nombre);
+                 
                 }
 
                 Restaurant.listaRestaurantes = lista;
-                System.Diagnostics.Debug.WriteLine("Salida Interna ");
                 return PartialView(Restaurant);
             }
 
-
-            System.Diagnostics.Debug.WriteLine("Salida Externa ");
             return PartialView();
         }
+
+        #endregion
 
         /// <summary>
         /// Método para el acceso a la interfaz de agregación de restaurantes.
@@ -75,32 +61,14 @@ namespace BOReserva.Controllers
         /// <returns>Retorna un objeto para renderizar la vista parcial.</returns>
         public ActionResult M10_GestionRestaurantes_Agregar()
         {
-         
+            ViewBag.Ciudad = FabricaVista.asignarItemsComboBox(cargarComboBoxLugar(), "Id", "Nombre");
+            ViewBag.Horarios = FabricaVista.asignarItemsComboBox(cargarComboBoxHorario(), "", "");
             return PartialView();
         }
 
 
 
-        [HttpGet]
-        public JsonResult verRestaurant(int id)
-        {
-          //  ViewBag.Ciudad = FabricaVista.asignarItemsComboBox(cargarComboBoxLugar(), "Id", "Nombre");
-            Entidad _lugar = FabricaEntidad.inicializarLugar(id, "");//Aqui se envia la clave foranea de lugar para realizar la busqueda
-            Comando<List<Entidad>> comando = (Comando<List<Entidad>>)FabricaComando.comandosRestaurant(FabricaComando.comandosGlobales.CONSULTAR, _lugar);
-            List<Entidad> restaurantes = comando.Ejecutar();
-            this.Restaurant = FabricaEntidad.inicializarRestaurant();
-            List<CRestauranteModelo> lista = FabricaEntidad.inicializarListaRestarant();
-
-            foreach (Entidad item in restaurantes)
-            {
-                lista.Add((CRestauranteModelo)item);
-            }
-
-           this.Restaurant.listaRestaurantes = lista;
-
-
-            return (Json(true, JsonRequestBehavior.AllowGet)); 
-        }
+ 
         /// <summary>
         /// Método para el acceso a la interfaz de modificación de restaurantes.
         /// </summary>
@@ -138,25 +106,27 @@ namespace BOReserva.Controllers
         /// </summary>
         /// <returns>Retorna un objecto tipo JsonResult que indica el éxito o fracaso de la operación.</returns>
         [HttpPost]
-        public JsonResult guardarRestaurante(CRestauranteModelo model)
+        public JsonResult guardarRestaurante(String Nombre,String Direccion,String Descripcion, String Telefono,int idLugar,String HoraIni,String HoraFin)
         {
+            System.Diagnostics.Debug.WriteLine("Hora del restaurant " + HoraIni);
+         
             //Chequeo de campos obligatorios para el formulario
-            if ((model.nombre == null) || (model.direccion == null) 
-                || (model.horarioApertura == null) || (model.horarioCierre == null) || (model.idLugar == -1))
-            {
-                Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                string error = "Error, campo obligatorio vacío";
-                return Json(error);
-            }
+            //if ((model.nombre == null) || (model.direccion == null) 
+            //    || (model.horarioApertura == null) || (model.horarioCierre == null) || (model.idLugar == -1))
+            //{
+            //    Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            //    string error = "Error, campo obligatorio vacío";
+            //    return Json(error);
+            //}
             manejadorSQL sql = new manejadorSQL();
             var salida = new CRestauranteModelo
             {
-                nombre = model.nombre,
-                direccion = model.direccion,
-                descripcion = model.descripcion,
-                horarioApertura = model.horarioApertura,
-                horarioCierre = model.horarioCierre,
-                idLugar = model.idLugar
+                //nombre = model.nombre,
+                //direccion = model.direccion,
+                //descripcion = model.descripcion,
+                //horarioApertura = model.horarioApertura,
+                //horarioCierre = model.horarioCierre,
+                //idLugar = model.idLugar
             };
          
             if (true)
@@ -169,6 +139,7 @@ namespace BOReserva.Controllers
                 string error = "Error agregando el restaurante.";
                 return Json(error);
             }
+            
         }
 
         /// <summary>
@@ -240,6 +211,7 @@ namespace BOReserva.Controllers
 
         }
 
+        #region Cargar Combobox ciudades
         public List<Lugar> cargarComboBoxLugar()
         {
             Comando<List<Entidad>> comando = (Comando<List<Entidad>>)FabricaComando.comandosVistaRestaurant(FabricaComando.comandoVista.CARGAR_LUGAR);
@@ -253,7 +225,19 @@ namespace BOReserva.Controllers
 
             }
             return lista;
-           
+        }
+        #endregion
+
+        public List<String> cargarComboBoxHorario()
+        {
+            Comando<List<String>> comando = (Comando<List<String>>)FabricaComando.comandosVistaRestaurant(FabricaComando.comandoVista.CARGAR_HORA);
+            List<String> horarios = comando.Ejecutar();
+
+            foreach (var item in horarios)
+            {
+                System.Diagnostics.Debug.WriteLine("Id del restaurant " + item);
+            }
+            return horarios;
         }
     }
 }
--- src/BOReserva/Scripts/Scripts_Restaurantes/llamadas_generales.js
+++ src/BOReserva/Scripts/Scripts_Restaurantes/llamadas_generales.js
@@ -38,10 +38,19 @@ $("#aceptarRestaurante").click(function (e) {
     console.log("Enviando...");
     e.preventDefault();
     e.stopImmediatePropagation();
-    var form = $("#formGuardarRestaurante");
+
+    var nombre = $("#Nombre").val();
+    var descripcion = $("#Descripcion").val();
+    var direccion = $("#Direccion").val();
+    var telefono = $("#Telefono").val();
+    var idlugar = $('#ComboLugar :selected').val();
+    var horaini = $('#ComboHoraIni :selected').text();
+    var horafin = $('#ComboHoraFin :selected').text();
+
     $.ajax({
         url: "gestion_restaurantes/guardarRestaurante",
-        data: form.serialize(),
+        data: { Nombre: nombre, Descripcion: descripcion, Direccion: direccion, Telefono: telefono, idLugar: idlugar, HoraIni: horaini, HoraFin: horafin },
+        dataType: "json",
         type: 'POST',
         success: function (data) {
             alert("El restaurante se agrego con exito.");
