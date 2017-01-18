d1c23c0636828a69598a803e2181884e2fe9810c
Rjus <rjus.prof@gmail.com>
2017-01-18 17:55:06 -0400

    M08
    Creación de los primeros comandos del módulo de automoviles

--- src/BOReserva/Controllers/PatronComando/FabricaComando.cs
+++ src/BOReserva/Controllers/PatronComando/FabricaComando.cs
@@ -49,6 +49,40 @@ namespace BOReserva.Controllers.PatronComando
         }
         #endregion
 
+        #region M08_Automoviles
+
+        public static Command<String> activarAutomovil(Entidad e)
+        {
+            return new M08.M08_COActivarAutomovil(e);
+        }
+
+        public static Command<String> agregarAutomovil(Entidad e)
+        {
+            return new M08.M08_COAgregarAutomovil(e);
+        }
+
+        public static Command<String> buscarAutomovil(Entidad e)
+        {
+            return new M08.M08_COBuscarAutomovil(e);
+        }
+
+        public static Command<String> desactivarAutomovil(Entidad e)
+        {
+            return new M08.M08_CODesactivarAutomovil(e);
+        }
+
+        public static Command<String> listarAutomovil(Entidad e)
+        {
+            return new M08.M08_COListarAutomovil(e);
+        }
+
+        public static Command<String> modificarAutomovil(Entidad e)
+        {
+            return new M08.M08_COModificarAutomovil(e);
+        }
+
+        #endregion
+
         public static Command<String> crearM13_AgregarRol(Entidad e)
         {
 
---
+++ src/BOReserva/Controllers/PatronComando/M08/M08_COBuscarAutomovil.cs
@@ -0,0 +1,29 @@
+﻿using BOReserva.Controllers.PatronComando;
+using BOReserva.DataAccess.DataAccessObject;
+using BOReserva.DataAccess.Domain;
+using BOReserva.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.Controllers.PatronComando.M08
+{
+    public class M08_COBuscarAutomovil : Command<String>
+    {
+                
+        #region Atributos
+        Entidad _objeto;
+        #endregion
+
+        #region Constructor
+        public M08_COBuscarAutomovil() { }
+
+        public M08_COBuscarAutomovil(Entidad _objeto)
+        {
+            this._objeto = _objeto;
+        }
+        #endregion
+
+    }
+}
