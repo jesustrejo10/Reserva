7f383696006410ce37f8de8a162508b347101231
Jesus <jesustrejo10@gmail.com>
2017-01-18 14:04:39 -0430

    pruebas con remota

--- src/BOReserva/DataAccess/DataAccessObject/DAO.cs
+++ src/BOReserva/DataAccess/DataAccessObject/DAO.cs
@@ -3,6 +3,7 @@ using BOReserva.DataAccess.Domain;
 using BOReserva.DataAccess.Model;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace BOReserva.DataAccess.DataAccessObject
 {
     abstract public class DAO : IDAO
     {
-        protected String _connexionString = "Data Source=sql5021.smarterasp.net;User ID=DB_A14EBC_reserva_admin;Password=ucabds1617a;";
+        protected String _connexionString = ConfigurationManager.ConnectionStrings["StringRemoto"].ConnectionString; 
         private SqlConnection conexion;
         // El String de conexion se encuentra en el archivo Web.config
         private SqlCommand comando;
