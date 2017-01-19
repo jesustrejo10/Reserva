6cf515dcf26f94099cefe8df061dbf1e35d89a15
DESKTOP-7EEOPD7\benjamin <bvpenaloza.11@gmail.com>
2017-01-19 01:06:47 -0400

    Conflictos Merge Develop FabricaComando

--- src/BOReserva/Controllers/PatronComando/FabricaComando.cs
+++ src/BOReserva/Controllers/PatronComando/FabricaComando.cs
@@ -1,210 +1,7 @@
-<<<<<<< HEAD
-﻿using BOReserva.DataAccess.Domain;
-using BOReserva.M10.Comando.gestion_restaurantes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace BOReserva.Controllers.PatronComando
-{
-    /// <summary>
-    /// Fabrica de todos los comandos de la aplicacion.
-    /// Esta clase es utilizada para instanciar a los comandos
-    /// </summary>
-    public class FabricaComando
-    {
-        /// <summary>
-        /// Metodo creado con la finalidad de instanciar el comando
-        /// M09_COAgregarHotel
-        /// </summary>
-        /// <param name="e">Recibe la una entidad de tipo Hotel</param>
-        /// <returns>Retorna un comando con el parametro adjuntado como atributo.</returns>
-        public static Command<String> crearM09AgregarHotel(Entidad e) {
-
-            return new M09_COAgregarHotel((Hotel)e);
-
-        }
-
-        /// <summary>
-        /// Metodo creado con la finalidad de instanciar el comando
-        /// M09_COVisualizarHoteles
-        /// <returns>Retorna la instancia del comando M09_COVisualizarHoteles.</returns>
-        public static Command<Dictionary<int, Entidad>> crearM09VisualizarHoteles()
-        {
-
-            return new M09_COVisualizarHoteles();
-
-        }
-
-        public static Command<String> crearM16_AgregarReclamo(Entidad e) 
-        {
-            return new M16_COAgregarReclamo((Reclamo)e);
-        }
-        
-
-        #region M04_Vuelo
-        public static Command<String> crearVuelo(Entidad vuelo)
-        {
-            return new M04.M04_COAgregarVuelo();
-        }
-        #endregion
-
-        #region M08_Automoviles
-
-        public static Command<String> activarAutomovil(Entidad e)
-        {
-            return new M08.M08_COActivarAutomovil(e);
-        }
-
-        public static Command<String> agregarAutomovil(Entidad e)
-        {
-            return new M08.M08_COAgregarAutomovil(e);
-        }
-
-        public static Command<String> buscarAutomovil(Entidad e)
-        {
-            return new M08.M08_COBuscarAutomovil(e);
-        }
-
-        public static Command<String> desactivarAutomovil(Entidad e)
-        {
-            return new M08.M08_CODesactivarAutomovil(e);
-        }
-
-        public static Command<String> listarAutomovil(Entidad e)
-        {
-            return new M08.M08_COListarAutomovil(e);
-        }
-
-        public static Command<String> modificarAutomovil(Entidad e)
-        {
-            return new M08.M08_COModificarAutomovil(e);
-        }
-
-        #endregion
-
-        public static Command<String> crearM13_AgregarRol(Entidad e)
-        {
-
-            return new M13_COAgregarRol((Rol)e);
-
-        }
-
-        public static Command<String> crearM13_AgregarRolPermiso(Entidad e)
-        {
-
-            return new M13_COAgregarRolPermiso((Rol)e);
-
-        }
-        #region M05_Boleto
-        public static Command<String> crearM05AgregarPasajero(Entidad e)
-        {
-            return new M05_COAgregarPasajero((Pasajero)e);
-
-        }
-
-        public static Command<String> crearM05CrearBoleto(Entidad e)
-        {
-            return new M05_COCrearBoleto((Boleto)e);
-        }
-        #endregion
-
-        #region Modulo 10 Gestion Restaurante
-
-        #region Comandos Generales 
-        /// <summary>
-        /// Metodo que recibe un comando para Crear, Actualizar, Eliminar o Consultar
-        /// la variable comando recibe comandosGlobales.CREAR, comandosGlobales.ELIMINAR
-        /// comandosGlobales.ACTUALIZAR, comandosGlobales.CONSULTAR 
-        /// </summary>
-        /// <param name="comando"></param>
-        /// <returns>regresa un tipo Objecto que debe ser casteado segun sea el caso</returns>
-        public static Object comandosRestaurant(comandosGlobales comando, comandoRestaurant comandoR, Entidad _objeto)
-        {
-            switch (comando)
-            {
-                case comandosGlobales.CREAR:
-                    return new M10_COCrearRestaurant(_objeto);
-
-                case comandosGlobales.ELIMINAR:
-                    return new M10_COEliminarRestaurant(_objeto);
-
-                case comandosGlobales.ACTUALIZAR:
-                    return new comandoActualizarRestaurant(_objeto);
-
-                case comandosGlobales.CONSULTAR:
-
-                    switch (comandoR)
-                    {
-                        case comandoRestaurant.NULO:
-                            break;
-                        case comandoRestaurant.CONSULTAR_ID:
-                            return new M10_COConsultarRestaurantId(_objeto);
-                    }
-                    return new M10_COConsultarRestaurant(_objeto);
-
-                default:
-                    return new M10_COConsultarRestaurant(_objeto);
-            }
-        }
-        #endregion
-        #region comandos de las interfaces Restaurant
-        public static Object comandosVistaRestaurant(comandoVista comando)
-        {
-            switch (comando)
-            {
-                case comandoVista.CARGAR_LUGAR:
-                    return new M10_COConsultarLugar();
-                case comandoVista.CARGAR_HORA:
-                    return new M10_COCargarHorario();
-                default:
-                    return new M10_COConsultarLugar();
-            }
-
-        }
-        #endregion
-
-
-        public enum comandoVista
-        {
-            CARGAR_LUGAR,
-            CARGAR_HORA
-        }
-
-        public enum comandoRestaurant
-        {
-            NULO,
-            CONSULTAR_ID
-        }
-
-        public enum comandosGlobales
-        {
-            CREAR,
-            ELIMINAR,
-            ACTUALIZAR,
-            CONSULTAR
-        }
-
-        public static List<Lugar> listaLugares(Lugar lugar)
-        {
-            List<Lugar> lista = new List<Lugar>();
-            lista.Add(lugar);
-            return lista;
-        }
-        #endregion
-
-    }
-=======
-﻿using BOReserva.Controllers.PatronComando;
-using BOReserva.Controllers.PatronComando.M09;
 using BOReserva.DataAccess.Domain;
 using BOReserva.M10.Comando.gestion_restaurantes;
-using BOReserva.Controllers.PatronComando;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace BOReserva.Controllers.PatronComando
 {
@@ -391,6 +188,40 @@ namespace BOReserva.Controllers.PatronComando
 
         #endregion
 
+        #region M08_Automoviles
+
+        public static Command<String> activarAutomovil(Entidad e)
+        {
+            return new M08.M08_COActivarAutomovil(e);
+        }
+
+        public static Command<String> agregarAutomovil(Entidad e)
+        {
+            return new M08.M08_COAgregarAutomovil(e);
+        }
+
+        public static Command<String> buscarAutomovil(Entidad e)
+        {
+            return new M08.M08_COBuscarAutomovil(e);
+        }
+
+        public static Command<String> desactivarAutomovil(Entidad e)
+        {
+            return new M08.M08_CODesactivarAutomovil(e);
+        }
+
+        public static Command<String> listarAutomovil(Entidad e)
+        {
+            return new M08.M08_COListarAutomovil(e);
+        }
+
+        public static Command<String> modificarAutomovil(Entidad e)
+        {
+            return new M08.M08_COModificarAutomovil(e);
+        }
+
+        #endregion
+
         #region Modulo 10 Gestion Restaurante
 
         #region Comandos Generales 
@@ -483,5 +314,4 @@ namespace BOReserva.Controllers.PatronComando
 
 
     }
->>>>>>> Develop
 }
