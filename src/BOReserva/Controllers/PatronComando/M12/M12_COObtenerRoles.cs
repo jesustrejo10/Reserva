097de90721ebd67d90cba9d9199dde16b24729f7
gabrielagonzalez6 <ggm.gaby06@gmail.com>
2017-01-19 12:16:00 -0400

    Cambios

---
+++ src/BOReserva/Controllers/PatronComando/M12/M12_COObtenerRoles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BOReserva.DataAccess.DataAccessObject;
+using BOReserva.DataAccess.DataAccessObject.InterfacesDAO;
+using BOReserva.DataAccess.Domain;
+using BOReserva.DataAccess.Domain.Contratos;
+
+namespace BOReserva.Controllers.PatronComando.M12
+{
+    public class M12_COObtenerRoles : Command<Dictionary<int, Entidad>>
+    {
+        public override Dictionary<int, Entidad> ejecutar()
+        {
+            IDAO daoRol = FabricaDAO.instanciarDAORol();
+            Dictionary<int, Entidad> roles = daoRol.ConsultarTodos();
+            Dictionary<int, Entidad> listaRoles = new Dictionary<int, Entidad>();
+
+            foreach (var rol in roles)
+            {
+                Rol r = (Rol)rol.Value;
+                listaRoles.Add(rol.Key, r);
+            }
+
+            return listaRoles;
+        }
+    }
+}
