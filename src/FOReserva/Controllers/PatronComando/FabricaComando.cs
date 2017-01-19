10bfa466a132e92ad5e7ddb3373da3d95c6539c0
azebac <jd.cabeza@gmail.com>
2017-01-19 00:53:46 -0400

    Finalizado agregar un reclamo via FO

--- src/FOReserva/Controllers/PatronComando/FabricaComando.cs
+++ src/FOReserva/Controllers/PatronComando/FabricaComando.cs
@@ -1,4 +1,5 @@
 ﻿using FOReserva.Controllers.PatronComando;
+using FOReserva.Controllers.PatronComando.M16;
 using FOReserva.DataAccess.Domain;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,18 @@ namespace FOReserva.Controllers.PatronComando
     public class FabricaComando
     {
         #region M16 GestionReclamos
+        /// <summary>
+        /// Metodo creado con la finalidad de instanciar el comando
+        /// M09_COAgregarReclamo
+        /// </summary>
+        /// <param name="e">Recibe la una entidad de tipo Reclamo</param>
+        /// <returns>Retorna un comando con el parametro adjuntado como atributo.</returns>
+        public static Command<String> crearM16AgregarReclamo(Entidad e)
+        {
 
+            return new M16_COAgregarReclamo((Reclamo)e);
+
+        }
         #endregion
     }
 }
--- src/FOReserva/DataAccess/Domain/FabricaEntidad.cs
+++ src/FOReserva/DataAccess/Domain/FabricaEntidad.cs
@@ -1,4 +1,5 @@
 ﻿using FOReserva.Models;
+using FOReserva.Models.Reclamos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,22 @@ namespace FOReserva.DataAccess.Domain
     /// </summary>
     public class FabricaEntidad
     {
+
         public static List<Entidad> asignarListaDeEntidades()
         {
             return new List<Entidad>();
         }
+        #region M16 Reclamos
+        public static Entidad InstanciarReclamo (CAgregarReclamo reclamo)
+        {
+            String _tituloReclamo = reclamo._tituloReclamo;
+            String _detalleReclamo = reclamo._detalleReclamo;
+            String _fechaReclamo = reclamo._fechaReclamo;
+            int _estadoReclamo = 1;
+            int _usuarioReclamo = reclamo._usuarioReclamo;
+            return new Reclamo(_tituloReclamo, _detalleReclamo, _fechaReclamo, _estadoReclamo, _usuarioReclamo);
+        }
+        #endregion
 
     }
 }
