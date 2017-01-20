93326b79d38bf7589acf821d59f56ecd78b3f6b3
drbrstudent <drbr20@hotmail.com>
2017-01-20 16:01:07 -0400

    Commit

---
+++ src/BOReserva/Excepciones/M10/ReservaExceptionM10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.Excepciones.M10
+{
+    public class ReservaExceptionM10
+    {
+        public ReservaExceptionM10(): base()
+        { }
+
+        public ReservaExceptionM10(string message): base(message)
+        {
+        }
+
+        public ReservaExceptionM10(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        public ReservaExceptionM10(string codigo, string message, Exception inner) : base(codigo, message, inner)
+        {
+        }
+    }
+}
