68f77f117a9ff6976110347b3a94294b676e8cf0
Lindsay201291 <Lindsay_201291@hotmail.com>
2017-01-19 21:57:56 -0400

    Empezando con DAO para los paquetes

---
+++ src/BOReserva/DataAccess/DataAccessObject/InterfacesDAO/IDAOPaquete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOReserva.DataAccess.DataAccessObject.InterfacesDAO
+{
+    interface IDAOPaquete : IDAO
+    {
+        //Métodos propios para los paquetes
+    }
+}
