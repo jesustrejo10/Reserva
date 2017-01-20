44404d75db1b18ca77de8c29340d10c6d4e4acce
Lindsay201291 <Lindsay_201291@hotmail.com>
2017-01-19 22:03:37 -0400

    iniciando patron comando para paquetes

---
+++ src/BOReserva/Controllers/PatronComando/M11/M11_COModificarPaquete.cs
@@ -0,0 +1,18 @@
+﻿using BOReserva.DataAccess.DataAccessObject;
+using BOReserva.DataAccess.DataAccessObject.InterfacesDAO;
+using BOReserva.DataAccess.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.Controllers.PatronComando.M11
+{
+    public class M11_COModificarPaquete
+    {
+        /// <summary>
+        /// Comando Modificar Paquete
+        /// </summary>
+        
+    }
+}
