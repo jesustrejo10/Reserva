6f3dd77a8f9cad719616b9ef6a1a9d530db19582
drbrstudent <drbr20@hotmail.com>
2017-01-20 01:55:59 -0400

    Arreglo de Vista restaurant

--- src/BOReserva/Scripts/cargadorMenu.js
+++ src/BOReserva/Scripts/cargadorMenu.js
@@ -311,7 +311,7 @@ jQuery(document).ready(function () {
         e.preventDefault();
         var url = '/gestion_restaurantes/M10_GestionRestaurantes_Ver';
         var method = 'GET';
-        var data = '';
+        var data = -1;
 
         $.ajax(
             {
