dd8e621e9fdeebb3c461b5375c6cb47dac4d835a
Lindsay201291 <Lindsay_201291@hotmail.com>
2017-01-20 00:31:16 -0400

    DAOPaquete en proceso

---
+++ src/BOReserva/DataAccess/DataAccessObject/M11/DAOPaquete.cs
@@ -0,0 +1,36 @@
+﻿using BOReserva.DataAccess.DataAccessObject.InterfacesDAO;
+using BOReserva.DataAccess.Domain;
+using BOReserva.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace BOReserva.DataAccess.DataAccessObject.M11
+{
+    public class DAOPaquete : DAO, IDAOPaquete
+    {
+        public DAOPaquete() {}
+
+        int IDAO.Agregar(Entidad e)
+        {
+            Paquete paquete = (Paquete)e;
+            SqlConnection conexion = Connection.getInstance(_connexionString);  //Para obtener la conexión a la 
+                                                                                //base de datos
+            try
+            {
+                conexion.Open();
+                SqlCommand query = conexion.CreateCommand(); //Se usa SqlCommand para las instrucciones que se 
+                                                             //ejecutan en una base de datos de SQL Server
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+            return 0;
+        }
+    }
+}
