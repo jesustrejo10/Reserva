e5245107d5fa7520adaa6b217a2162baa298e72f
Carlos Herrera <carlosdhm6@gmail.com>
2017-01-21 11:48:17 -0400

    Corrigiendo errores de compilacion CRUCEROS

--- src/BOReserva/DataAccess/Domain/M14/Cabina.cs
+++ src/BOReserva/DataAccess/Domain/M14/Cabina.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace BOReserva.DataAccess.Domain.M14
+namespace BOReserva.DataAccess.Domain
 {
     public class Cabina : Entidad
     {
