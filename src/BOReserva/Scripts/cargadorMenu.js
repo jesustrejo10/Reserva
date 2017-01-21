74cc664477d6d21d3a983c55f1a0ac040df1fdd5
manuelpimentel <manuelpimentel16@gmail.com>
2017-01-21 14:25:06 -0400

    Para probar visualizar

--- src/BOReserva/Scripts/cargadorMenu.js
+++ src/BOReserva/Scripts/cargadorMenu.js
@@ -848,6 +848,28 @@ jQuery(document).ready(function () {
             });
     });
 
+    $("#m24_listarcabinas").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_cruceros/M24_ListarCabinas';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
     $("#m24_agregarcamarote").click(function (e) {
         e.preventDefault();
         var url = '/gestion_cruceros/M24_AgregarCamarote';
@@ -869,6 +891,29 @@ jQuery(document).ready(function () {
                 }
             });
     });
+
+    $("#m24_listarcamarotes").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_cruceros/M24_ListarCamarotes';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
 
     $("#m24_agregaritinerario").click(function (e) {
         e.preventDefault();
