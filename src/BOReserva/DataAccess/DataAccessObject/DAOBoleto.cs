9d88f282248347702ac1710f9c3374395499b1e8
Carlos Herrera <carlosdhm6@gmail.com>
2017-01-22 18:15:47 -0400

    add comentario daoBoleto

--- src/BOReserva/DataAccess/DataAccessObject/DAOBoleto.cs
+++ src/BOReserva/DataAccess/DataAccessObject/DAOBoleto.cs
@@ -10,9 +10,14 @@ using System.Diagnostics;
 
 namespace BOReserva.DataAccess.DataAccessObject
 {
+    /// <summary>
+    /// Clase encargada del Acceso a DB para el objeto Boleto
+    /// </summary>
     public class DAOBoleto : DAO, IDAOBoleto
     {
-
+        /// <summary>
+        /// Constructor Vacio
+        /// </summary>
         public DAOBoleto() { }
 
         int IDAO.Agregar(Entidad e)
