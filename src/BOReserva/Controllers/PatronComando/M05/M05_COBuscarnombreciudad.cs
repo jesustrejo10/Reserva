e094236c189cec5c112f82776f4ecd5904a297e0
Carlos Herrera <carlosdhm6@gmail.com>
2017-01-22 23:17:49 -0400

    comentarios comando buscar nombre de ciudad

--- src/BOReserva/Controllers/PatronComando/M05/M05_COBuscarnombreciudad.cs
+++ src/BOReserva/Controllers/PatronComando/M05/M05_COBuscarnombreciudad.cs
@@ -5,11 +5,18 @@ using System;
 
 namespace BOReserva.Controllers.PatronComando
 {
+    /// <summary>
+    /// Clase comando para la busqueda de la ciudad
+    /// </summary>
     public class M05_COBuscarnombreciudad : Command<String>
     {
 
         int idciudad;
 
+        /// <summary>
+        /// Comando para la busqueda del nombre de una ciudad
+        /// </summary>
+        /// <param name="e"></param>
         public M05_COBuscarnombreciudad(Entidad e)
         {
             this.idciudad = e._id;
