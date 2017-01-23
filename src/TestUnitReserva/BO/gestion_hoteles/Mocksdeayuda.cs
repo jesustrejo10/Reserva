3520f04b1f544ac2e5d93d263fe692c1b92ce171
luiisperez <luis.alejandro120895@gmail.com>
2017-01-22 23:42:26 -0400

    Pruebas completas (creo)

---
+++ src/TestUnitReserva/BO/gestion_hoteles/Mocksdeayuda.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUnitReserva.BO.gestion_hoteles
+{
+    class Mocksdeayuda
+    {
+        /// <summary>
+        /// Metodo para crear una SQLException
+        /// </summary>
+        /// <returns></returns>
+        public static SqlException MakeSqlException()
+        {
+            SqlException exception = null;
+            try
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=.;Database=GUARANTEED_TO_FAIL;Connection Timeout=1");
+                conn.Open();
+            }
+            catch (SqlException ex)
+            {
+                exception = ex;
+            }
+            return (exception);
+        }
+    }
+}
