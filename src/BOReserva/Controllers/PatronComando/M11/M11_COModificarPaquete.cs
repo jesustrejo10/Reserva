53670fbd083ac3ea2da1f8691ab0162447402152
Karolyn <lamitologica@gmail.com>
2017-01-23 09:34:52 -0400

    Eliminada Excepción.

--- src/BOReserva/Controllers/PatronComando/M11/M11_COModificarPaquete.cs
+++ src/BOReserva/Controllers/PatronComando/M11/M11_COModificarPaquete.cs
@@ -4,6 +4,7 @@ using BOReserva.DataAccess.Domain;
 using BOReserva.Excepciones.M11;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -15,7 +16,7 @@ namespace BOReserva.Controllers.PatronComando.M11
     public class M11_COModificarPaquete : Command<String>
     {
         Paquete _paquete;
-        //int _idmodificar;
+        int _idmodificar;
 
         /// <summary>
         /// Constructor de la clase
@@ -25,17 +26,19 @@ namespace BOReserva.Controllers.PatronComando.M11
         public M11_COModificarPaquete(Entidad paquete, int id)
         {
             this._paquete = (Paquete) paquete;
-            this._paquete._id = id;
+            this._idmodificar = id;
         }
 
         public override String ejecutar()
         {
+            Debug.WriteLine("ENTRÓ A EJECUTAR");
+            Debug.WriteLine("ENTRÓ A EJECUTAR " + _paquete._nombrePaquete);
+
             try
             {
-                IDAO daoPaquete = FabricaDAO.instanciarDaoPaquete();
-                Entidad test = daoPaquete.Modificar(_paquete);
-                Paquete paquete = (Paquete)test;
-                return paquete._nombrePaquete;
+                IDAOPaquete daoPaquete = FabricaDAO.instanciarDaoPaquete();
+                int test = daoPaquete.Modificar(_paquete,_idmodificar);
+                return test.ToString();
             }
             catch (ReservaExceptionM11 ex)
             {
