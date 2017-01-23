c883b827547bdfc424a838088c10b3d6440a7cbd
gabrielagonzalez6 <ggm.gaby06@gmail.com>
2017-01-23 01:29:57 -0400

    Test

--- src/TestUnitReserva/BO/gestion_usuario/TestUsuario.cs
+++ src/TestUnitReserva/BO/gestion_usuario/TestUsuario.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using BOReserva.Servicio;
 using System.Diagnostics;
 using System.Data.SqlClient;
 using BOReserva.Models.gestion_usuarios;
+using BOReserva.Servicio;
 using BOReserva.DataAccess.DAO;
 using BOReserva.DataAccess.Domain;
 using BOReserva.Controllers.PatronComando.M12;
@@ -22,143 +22,167 @@ using System.Web.Mvc;
 
 namespace TestUnitReserva.BO.gestion_usuarios
 {
+    /// <summary>
+    /// Clase encargada de realizar las pruebas unitarios del modulo Usuarios de BO
+    /// </summary>
     [TestFixture]
     public class TestUsuario
     {
-        //#region Atributos
-
-        //public CUsuario elUsuario;
-        //List<ListarUsuario> lista;
-        //PersistenciaUsuario prueba;
-        //int id;
-        //ListarUsuario liastarusu;
-        //bool revision;
-
-        //#endregion
-
-        //#region SetUp And TearDown
-        ///// <summary>
-        ///// SetUp para las pruebas de DAOUsuario
-        ///// </summary>
-        //[SetUp]
-        //public void init()
-        //{
-        //    // BOReserva.Models.gestion_usuarios.
-        //    elUsuario = new CUsuario("nombre", "apellido", "contraseña456", "correosssssss@gmail.com", "activo", 1);
-        //    prueba = new PersistenciaUsuario();
-        //    liastarusu = new ListarUsuario();
-        //}
-
-        ///// <summary>
-        ///// TearDown para las pruebas de DAOUsuario
-        ///// </summary>
-        //[TearDown]
-        //public void clean()
-        //{
-        //    elUsuario = null;
-        //    prueba = null;
-        //    lista = null;
-        //}
-        //#endregion
-        //#region Test
-        //[Test]
-        //public void TestListar()
-        //{
-        //    lista = prueba.ListaUsuarios();
-        //    Assert.IsNotNull(lista);
-        //}
-
-        //[Test]
-        //public void TestModificar()
-        //{
-        //    bool primero = prueba.AgregarUsuario(elUsuario);
-        //    CUsuario elUsuario2 = new CUsuario("hola", "cambio", "1234678tghfhf", "correosssssss@gmail.com", "activo", 1);
-        //    id = prueba.ultimoUsuarioID();
-        //    bool segundo = prueba.ModificarUsuario(elUsuario2, id);
-        //    CUsuario elUsuario3 = prueba.consultarUsuario(id);
-        //    Assert.AreEqual(elUsuario2.nombreUsuario, elUsuario3.nombreUsuario);
-        //    Assert.AreEqual(elUsuario2.correoUsuario, elUsuario3.correoUsuario);
-        //    Assert.AreEqual(elUsuario2.apellidoUsuario, elUsuario3.apellidoUsuario);
-        //    Assert.AreEqual(elUsuario2.activoUsuario, elUsuario3.activoUsuario);
-        //    revision = prueba.eliminarUsuario(id);
-        //}
-
-        //[Test]
-        //public void TestAgregar()
-        //{
-        //    int numero1 = prueba.contarUsuario();
-        //    revision = prueba.AgregarUsuario(elUsuario);
-        //    int numero2 = prueba.contarUsuario();
-        //    Assert.AreEqual(numero1+1,numero2);
-        //    id = prueba.ultimoUsuarioID();
-        //    revision = prueba.eliminarUsuario(id);
-        //}
-        //[Test]
-        //public void TestBorrar()
-        //{
-        //    int numero1 = prueba.contarUsuario();
-        //    bool primero = prueba.AgregarUsuario(elUsuario);
-        //    id = prueba.ultimoUsuarioID();
-        //    bool segundo = prueba.eliminarUsuario(id);
-        //    int numero2 = prueba.contarUsuario();
-        //    Assert.AreEqual(numero1,numero2);
-        //}
-        //[Test]
-        //public void TestConsultar()
-        //{
-        //    revision = prueba.AgregarUsuario(elUsuario);
-        //    id = prueba.ultimoUsuarioID();
-        //    elUsuario = prueba.consultarUsuario(id);
-        //    Assert.IsNotNull(elUsuario);
-        //    Assert.IsNotNull(elUsuario.apellidoUsuario);
-        //    Assert.IsNotNull(elUsuario.correoUsuario);
-        //    Assert.IsNotNull(elUsuario.nombreUsuario);
-        //    revision = prueba.eliminarUsuario(id);
-        //}
-        //#endregion
-
-    //    private Usuario mockUsuario;
-    //    private Usuario mockUsuario2;
-    //    private Rol mockRol;
 
 
-    //    IDAO prueba;
-    //    DAOUsuario daoUsuario;
+        private Usuario mockUsuario;
+        private Usuario mockUsuario2;
+        private Rol mockRol;
 
-    //    BOReserva.Controllers.gestion_usuariosController controlador = new BOReserva.Controllers.gestion_usuariosController();
 
-    //    [SetUp]
-    //    public void Before()
-    //    {
+        IDAO prueba;
+        IDAO daoUsuario;
 
-    //        mockRol = new Rol(1, "Administrador");
-    //        mockUsuario = new Usuario("Ana", "Duran", "ana@reserva.com", "12345a", mockRol, System.DateTime.Parse("yyyy-MM-dd"), "Activo");
-    //        mockUsuario2 = new Usuario(999, "Ana", "Duran", "ana@reserva.com", "12345a", mockRol, System.DateTime.Parse("yyyy-MM-dd"), "Activo");
+        BOReserva.Controllers.gestion_usuariosController controlador = new BOReserva.Controllers.gestion_usuariosController();
 
-    //        daoUsuario = new DAOUsuario();
+
+        /// <summary>
+        /// Metodo que se ejecuta antes de ejecutar todas las pruebas 
+        /// 
+        /// </summary>
+        [SetUp]
+        public void Before()
+        {
+            DateTime fecha = DateTime.Now;
+
+            mockRol = new Rol(1, "Administrador");
+            mockUsuario = new Usuario("Ana", "Duran", "ana@reserva.com", "12345a", mockRol, fecha, "Activo");
+            //mockUsuario2 = new Usuario(999, "Ana", "Duran", "ana@reserva.com", "12345a", mockRol,null, "Activo");
+
+            daoUsuario = new DAOUsuario();
 
 
 
-    //    }
+        }
 
-    //    [TearDown]
-    //    public void After()
-    //    {
-    //        mockUsuario = null;
-    //        mockUsuario2 = null;
-    //        mockRol = null;
-    //    }
 
-    //    [Test]
-    //    public void M12_DaoUsuarioInsertarUsuario()
-    //    {
-    //        //Probando caso de exito de la prueba
-    //        int resultadoAgregar = daoUsuario.Agregar(mockUsuario);
-    //        Assert.AreEqual(resultadoAgregar, 1);
-    //        //Probando caso de fallo
-    //        int resultadoAgregarIncorrecto = daoUsuario.Agregar(null);
-    //        Assert.AreEqual(resultadoAgregarIncorrecto, 0);
-    //    }
+        /// <summary>
+        /// Metodo que se ejecuta cada vez que se termina de correr una prueba, se
+        /// encargar de limpiar las variables utilizadas
+        /// </summary>
+        [TearDown]
+        public void After()
+        {
+            mockUsuario = null;
+            mockUsuario2 = null;
+            mockRol = null;
+        }
+
+
+        /// <summary>
+        /// Metodo que prueba que se inserte un ususario a la BD
+        /// </summary>
+        [Test]
+        public void M12_DaoUsuarioInsertarUsuario()
+        {
+            //Probando caso de exito de la prueba
+            int resultadoAgregar = daoUsuario.Agregar(mockUsuario);
+            Assert.AreEqual(resultadoAgregar, 1);
+            //Probando caso de fallo
+            int resultadoAgregarIncorrecto = daoUsuario.Agregar(null);
+            Assert.AreEqual(resultadoAgregarIncorrecto, 0);
+        }
+
+
+        /// <summary>
+        /// Metodo que prueba que se modifique correctamente un usuario
+        /// </summary>
+        [Test]
+        public void M12_DaoUsuarioModificar()
+        {
+            daoUsuario.Agregar(mockUsuario);
+            M12_COModificarUsuario prueba = new M12_COModificarUsuario(mockUsuario, 434);
+            string test = prueba.ejecutar();
+        }
+
+        /// <summary>
+        /// Metodo que prueba que se elimine correctamente un usuario
+        /// </summary>
+        [Test]
+        public void M12_DaoUsuarioEliminarUsuario()
+        {
+            daoUsuario.Agregar(mockUsuario);
+            M12_COEliminarUsuario prueba = new M12_COEliminarUsuario(mockUsuario, 434);
+            string test = prueba.ejecutar();
+        }
+
+
+        /// <summary>
+        /// Metodo que prueba que se logre consultar todos los usuarios de la BD
+        /// </summary>
+        [Test]
+        public void M12_DaoUsuarioConsultarTodos()
+        {
+            Dictionary<int, Entidad> usuario = daoUsuario.ConsultarTodos();
+            Assert.NotNull(usuario);
+        }
+
+
+        /// <summary>
+        /// Metodo que prueba que se cambia el estatus de un usuario a Activo
+        /// </summary>
+        [Test]
+        public void M12_activateUsuario()
+        {
+            daoUsuario.Agregar(mockUsuario);
+            gestion_usuariosController prueba = new gestion_usuariosController();
+            JsonResult probar = prueba.activarUsuario(434, "Activo");
+            Assert.IsInstanceOf(typeof(JsonResult), probar);
+        }
+
+
+        /// <summary>
+        /// Metodo que prueba que se cambiar el estatus de un usuario a Inactivo
+        /// </summary>
+        [Test]
+        public void M12_desactivateUsuario()
+        {
+            daoUsuario.Agregar(mockUsuario);
+            gestion_usuariosController prueba = new gestion_usuariosController();
+            JsonResult probar = prueba.activarUsuario(434, "Inactivo");
+            Assert.IsInstanceOf(typeof(JsonResult), probar);
+        }
+
+
+        /// <summary>
+        /// Metodo que prueba que el visualizar ususarios
+        /// </summary>
+        [Test]
+        public void M12_VisualizarUsuario()
+        {
+            M12_COVisualizarUsuarios prueba = new M12_COVisualizarUsuarios();
+            Dictionary<int, Entidad> mapUsuario = prueba.ejecutar();
+        }
+
+
+        /// <summary>
+        /// Metodo que prueba que se inserta un usuario pasando por los patrones
+        /// </summary>
+        [Test]
+        public void M12_guardarUsuario()
+        {
+            DateTime fecha = DateTime.Now;
+            //Dictionary openWith = new Dictionary<int, Entidad>();
+            CAgregarUsuario model = new BOReserva.Models.gestion_usuarios.CAgregarUsuario();
+            model._nombre = "Maria";
+            model._apellido = "Rodriguez";
+            model._correo = "maria@reserva.com";
+            model.contraseñaUsuario = ("12345a");
+            model._activo = "activo";
+            model._fechaCreacion = fecha;
+            // model._rols = openWith;
+
+
+            gestion_usuariosController prueba = new gestion_usuariosController();
+            JsonResult probarjsonresult = prueba.guardarUsuario(model);
+            Assert.IsInstanceOf(typeof(JsonResult), probarjsonresult);
+        }
 
     }
+
 }
